32705c8fec48aa59d124d8cb088db7e0af722090
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-12-01 12:05:30 -0800

    fixed failing videos controller tests

--- test/controllers/videos_controller_test.rb
+++ test/controllers/videos_controller_test.rb
@@ -73,7 +73,7 @@ describe VideosController do
   describe "create" do
     let(:video_params) {
       {
-          video_hash: {
+          video: {
               title: "Alf the movie",
               overview: "The most early 90s movie of all time",
               release_date: "2020-12-25",
@@ -93,19 +93,11 @@ describe VideosController do
 
     it "will respond with bad request and errors for an invalid movie" do
       # Arrange
-      video_hash = {
-        title: "Alf the movie",
-        overview: "The most early 90s movie of all time",
-        release_date: "December 16th 2025",
-        total_inventory: 6,
-        available_inventory: 6
-      }
-  
-      video_hash[:title] = nil
+      video_params[:video][:title] = nil
   
       # Assert
       expect {
-        post videos_path, params: video_hash
+        post videos_path, params: video_params
       }.wont_change "Video.count"
       body = JSON.parse(response.body)
 
