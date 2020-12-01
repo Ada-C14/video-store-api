042942079d944c0e6591f8a2e3efde0cdc65b2aa
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-12-01 10:05:46 -0800

    add resources and controller actions for customer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'customers/index'
-  get 'customers/show'
-  get 'customers/create'
+  resources :customers, only: [:index, :show, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
