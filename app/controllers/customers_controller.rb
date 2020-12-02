f1c3373a8ced7234a8e5ef2a75feac3f509882b0
KayKay-git <kayalew@yahoo.com>
2020-12-01 21:27:32 -0500

    Logic for Customers index and show action methods

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,18 @@
 class CustomersController < ApplicationController
   def index
+    customers = Customer.all
+    render json: customers.as_json(except: [:created_at, :updated_at]), status: :ok
   end
 
   def show
+    customer = Customer.find_by(id: params[:id])
+    if customer.nil?
+      render json: {
+        ok: false,
+        message: 'Not Found'
+      }, status: :not_found
+      return
+    end
+    render json: customer.as_json(except: [:created_at, :updated_at]), status: :ok
   end
 end
