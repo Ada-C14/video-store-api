9c7a5a363eb59b82f157ad9682ad49c48baf2acc
KayKay-git <kayalew@yahoo.com>
2020-12-01 20:18:44 -0500

    Added Controllers, models, routes and actions for customer, video, rentals

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'rentals/create'
+  get 'videos/index'
+  get 'videos/show'
+  get 'customers/index'
+  get 'customers/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ test/controllers/rentals_controller_test.rb
@@ -0,0 +1,9 @@
+require "test_helper"
+
+describe RentalsController do
+  it "must get create" do
+    get rentals_create_url
+    must_respond_with :success
+  end
+
+end
