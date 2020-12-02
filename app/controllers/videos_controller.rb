97935ed0f5c4a1b1de43de0dba0cd834169b74ac
Ida <ida.goitom@yahoo.com>
2020-12-01 18:17:23 -0800

    added new migration for customers

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,10 @@
 class VideosController < ApplicationController
   def index
+    videos = Video.all.order(:title)
+
+    render json: videos.as_json(only: [:id, :title, :overview, :release_date, :total_inventory,
+                                     :available_inventory]),
+           status: :ok
   end
 
   def zomg
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'customers/index'
-  get "/zomg", to: "videos#zomg"
+  resources :videos, only: [:index]
+  # get "/zomg", to: "videos#zomg"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
