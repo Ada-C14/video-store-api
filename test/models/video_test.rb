d1933fbe2bee41f0bb96ef1198bf26f21fa7fff2
Beatrice T <be@trice.codes>
2020-12-02 15:49:56 -0800

    Changed video model test to use helper methods

--- test/models/video_test.rb
+++ test/models/video_test.rb
@@ -20,69 +20,55 @@ describe Video do
         video.title = nil
 
         expect(video.valid?).must_equal false
-        expect(video.errors.messages).must_include :title
-        expect(video.errors.messages[:title]).must_include "can't be blank"
+        check_invalid(model: video, attribute: :title)
       end
 
       it "is invalid without an overview" do
         video.overview = nil
 
         expect(video.valid?).must_equal false
-        expect(video.errors.messages).must_include :overview
-        expect(video.errors.messages[:overview]).must_include "can't be blank"
+        check_invalid(model: video, attribute: :overview)
       end
 
       it "is invalid without a release date" do
         video.release_date = nil
-
-        expect(video.valid?).must_equal false
-        expect(video.errors.messages[:release_date]).must_include "can't be blank"
+        check_invalid(model: video, attribute: :release_date)
       end
 
       it "is invalid if release date is in the future" do
         video.release_date = DateTime.now + 1
-
-        expect(video.valid?).must_equal false
+        check_invalid(model: video, attribute: :release_date)
       end
 
       it "is invalid if available inventory is nil" do
         video.available_inventory = nil
+        check_invalid(model: video, attribute: :available_inventory)
 
-        expect(video.valid?).must_equal false
-        expect(video.errors.messages[:available_inventory]).must_include "can't be blank"
       end
 
       it "is valid with 0 or more (non-negative) available inventory" do
         video.available_inventory = 0
-
         expect(video.valid?).must_equal true
       end
 
       it "is invalid with a negative available inventory" do
         video.available_inventory = -1
-
-        expect(video.valid?).must_equal false
-        expect(video.errors.messages[:available_inventory]).must_include "must be greater than or equal to 0"
+        check_invalid(model: video, attribute: :available_inventory)
       end
 
       it "is invalid if total inventory is nil" do
         video.total_inventory = nil
-
-        expect(video.valid?).must_equal false
-        expect(video.errors.messages[:total_inventory]).must_include "can't be blank"
+        check_invalid(model: video, attribute: :total_inventory)
       end
 
       it "is valid with 0 or more (non-negative) total inventory" do
         video.total_inventory = 0
-
         expect(video.valid?).must_equal true
       end
 
       it "is invalid with a negative total inventory" do
         video.total_inventory = -1
-
-        expect(video.valid?).must_equal false
-        expect(video.errors.messages[:total_inventory]).must_include "must be greater than or equal to 0"
+        check_invalid(model: video, attribute: :total_inventory)
       end
     end
   end
