d40a1458d3cdfd48d9374ef6991c43f23720770c
Tram Bui <pbui17@cmc.edu>
2020-12-02 10:42:25 -0600

    added validations and writing tests

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,8 @@
 class Customer < ApplicationRecord
+  # unique name?
+  # name must be present?
+  # what else do we need to be present?
+  # videos_checked_out_count must be positive number
+  validates :name, presence: true
+  validates :videos_checked_out_count, numericality: { greater_than: 0 }
 end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,7 +1,24 @@
 require "test_helper"
 
 describe Customer do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  before do
+    @customer = customers(:customer_one)
+  end
+
+  it "can be instiantiated when the fields are present" do
+    expect(@customer.valid?).must_equal true
+  end
+
+  it "responds to the fields" do
+    fields = [:name, :registered_at, :postal_code, :videos_checked_out_count]
+
+    fields.each do |field|
+      expect(@customer).must_respond_to field
+    end
+  end
+
+  describe "validations" do
+
+
+  end
 end
