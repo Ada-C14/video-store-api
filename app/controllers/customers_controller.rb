d5a75f02effb6327d6c5a284513c4a892806049d
MonaRahmani <monarahmanimehr@gmail.com>
2020-12-01 18:24:30 -0800

    added index action for customer

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,8 @@
 class CustomersController < ApplicationController
-  def index
+  CUSTOMER_FIELDS = ["id", "name", "registered_at", "postal_code", "phone", "videos_checked_out_count"]
 
+  def index
+    customers = Customer.all.order(:name)
+    render json: customers.as_json(only: CUSTOMER_FIELDS), status: :ok
   end
 end
