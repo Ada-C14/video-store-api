e262325e4c0563eeb4d34c1bb13693333caaeae3
Sophie Messing <sophie.e.messing@gmail.com>
2020-12-01 16:57:42 -0800

    created rentals model and added has_many and belongs_to relationships

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_003704) do
+ActiveRecord::Schema.define(version: 2020_12_02_005516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 2020_12_02_003704) do
     t.string "postal_code"
     t.string "phone"
     t.integer "videos_checked_out_count"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "rentals", force: :cascade do |t|
+    t.integer "customer_id"
+    t.integer "video_id"
+    t.date "due_date"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
