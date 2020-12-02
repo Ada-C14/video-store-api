ce2ca53468363f47db847e595ba42bcf87ebecbd
christinaminh <christina.minh@gmail.com>
2020-12-01 22:15:43 -0800

    Started tests for customer model validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,8 @@
 class Customer < ApplicationRecord
   has_many :rentals
   has_many :videos, through: :rentals
+
+  validates_presence_of :name, :registered_at, :videos_checked_out_count
+
+  validates :videos_checked_out_count, numericality: { greater_than_or_equal_to: 0 }
 end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,7 +1,29 @@
 require "test_helper"
 
 describe Customer do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "validations" do
+    it "is valid when required fields are present" do
+
+    end
+
+    it "is invalid without a name" do
+
+    end
+
+    it "is invalid without a registration date" do
+
+    end
+
+    it "is valid with 0 or more (non-negative) videos checked out" do
+
+    end
+
+    it "is invalid with a negative videos checked out" do
+
+    end
+
+    it "is invalid if videos checked out is nil" do
+
+    end
+  end
 end
