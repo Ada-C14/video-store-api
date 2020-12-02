53edfbd78de23369b81765d5c8b83049545cf4b5
KayKay-git <kayalew@yahoo.com>
2020-12-01 22:49:53 -0500

    Added nominal and edge tests for show action, pass

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,7 @@
 class CustomersController < ApplicationController
   def index
     customers = Customer.all
-    render json: customers.as_json(except: [:created_at, :updated_at]), status: :ok
+    render json: customers.as_json(only: [:id, :name, :phone, :postal_code, :registered_at, :videos_checked_out_count]), status: :ok
   end
 
   def show
@@ -13,6 +13,6 @@ class CustomersController < ApplicationController
       }, status: :not_found
       return
     end
-    render json: customer.as_json(except: [:created_at, :updated_at]), status: :ok
+    render json: customer.as_json(only: [:id, :name, :phone, :postal_code, :registered_at, :videos_checked_out_count]), status: :ok
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'videos/show'
 
   resources :customers, only: [:index, :show]
+  post 'customers/create'
   # get 'customers/index'
   # get 'customers/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
