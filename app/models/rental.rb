94e82f3496d2c8840e2e07ea2ecf5410b2b63d71
Genevieve Neely <genevieve.hood@gmail.com>
2020-12-02 11:13:15 -0800

    rental model validation tests

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,9 @@
 class Rental < ApplicationRecord
   belongs_to :customer
   belongs_to :video
+
+  validates :customer, :video, :due_date, presence: true
+  # validates :return_date, presence: true, on: :update
+
+
 end
