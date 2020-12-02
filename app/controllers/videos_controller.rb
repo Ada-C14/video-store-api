e254dbed9e1448c63617b3663c8091831552fa07
roshni-patel <rpatel01@wesleyan.edu>
2020-12-02 12:19:06 -0600

    Working on fixing failing smoke tests

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -8,18 +8,21 @@ class VideosController < ApplicationController
     video = Video.find_by(id: params[:id])
 
     if video.nil?
-      render json: {ok: false, message: "Not Found"}, status: :not_found
+      render json: {
+          # ok: false,
+          errors: ["Not Found"]
+      }, status: :not_found
       return
     end
 
-    render json: video.as_json(only: [:id, :title, :release_date, :available_inventory]), status: :ok
+    render json: video.as_json(only: [:title, :overview, :release_date, :total_inventory, :available_inventory]), status: :ok
   end
 
   def create
     video = Video.new(video_params)
 
     if video.save
-      render json: video.as_json(only: [:id, :title, :release_date, :available_inventory]), status: :created
+      render json: video.as_json(only: [:id]), status: :created
       return
     else
       render json: {
