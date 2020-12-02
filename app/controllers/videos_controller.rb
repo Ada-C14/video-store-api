d2a4387f0e7ca4dadc644198083aa0bb4b8df2c9
Jasmine <jasylop@gmail.com>
2020-12-02 10:25:50 -0800

    added index and show

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,2 +1,18 @@
 class VideosController < ApplicationController
+
+  def index
+    videos = Video.all.to_json(only: [:id, :title, :release_date, :available_inventory])
+    render json: videos, status: :ok
+  end
+
+  def show
+    video = Video.find_by(id: params[:id])
+    if video
+      video = video.as_json(only: [:title, :overview, :release_date, :total_inventory, :available_inventory])
+      render json: video, status: :ok
+    else
+      render json: {ok: false, errors: "Not Found"}
+    end
+  end
+
 end
