a14c8a95b75c74e12096b710d62a3c78e5acc147
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-12-01 17:19:39 -0800

    create Rental model and implement has_many-through relationships

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,6 @@
 class Customer < ApplicationRecord
+  has_many :rentals
+  has_many :videos, :through => :rentals
+
   validates :name, presence: true
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,6 @@
 class Video < ApplicationRecord
+  has_many :rentals
+  has_many :customers, :through => :rentals
+
   validates :title, presence: true
 end
