fdffa34e6d4c0e42659d320f17a479b69d9ca73d
dnsrocha <drocha@gmail.com>
2020-12-02 15:45:21 -0800

    added actions for videos controller; added video routes

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,27 @@
 class VideosController < ApplicationController
   def index
+    @movies = Video.all
+    movies = Video.all.as_json(only: [:id, :title, :overview, :release_date, :inventory])
+    render json: movies, status: :ok
   end
 
   def show
+    movie = Video.find_by(id: params[:id])
+
+    if movie
+      render json: movie.as_json(only: [:id, :title, :overview, :release_date, :inventory])
+    else
+      not_found
+    end
   end
+end
+
+# def create
+#
+# end
+
+private
+
+def video_params
+  params.require(:video).permit(:title, :overview, :release_date, :inventory)
 end
