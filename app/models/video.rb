4c354609049c9ca85cb3d856da3e4a44fb7a67f2
Pauline Chane <pauline.chane@gmail.com>
2020-12-01 18:52:34 -0800

    added model validations to videos

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,16 @@
 class Video < ApplicationRecord
   has_many :customers, through: :rentals
   has_many :rentals
+
+  validates :title, presence: true,
+                    uniqueness: true
+  validates :overview, presence: true
+  validates :release_date, presence: true
+  validates :total_inventory, presence: true,
+                              numericality: {only_integer: true,
+                                             greater_than: 0}
+  validates :available_inventory, presence: true,
+                                  numericality: {only_integer: true,
+                                                 greater_than_or_equal_to: 0,
+                                                 less_than_or_equal_to: :total_inventory}
 end
