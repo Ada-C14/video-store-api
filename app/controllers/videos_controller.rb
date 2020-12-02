6d2debfa998441a05dabbf3e72610120bf967c75
MonaRahmani <monarahmanimehr@gmail.com>
2020-12-01 19:55:34 -0800

    added video show action

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -5,4 +5,26 @@ class VideosController < ApplicationController
     render json: videos.as_json(only: [:id, :title, :release_date, :available_inventory]),
            status: :ok
   end
+
+  def show
+    video = Video.find_by(id: params[:id])
+
+    if video.nil?
+      render json: {
+          errors:
+              [
+                  "Not Found"
+              ]
+
+      }, status: :not_found
+      return
+    end
+    render json: video.as_json(only: [:title, :overview, :release_date, :available_inventory, :total_inventory]), status: :ok
+  end
+
+  private
+  def pet_params
+    params.require(:video).permit(:title, :overview, :release_date, :available_inventory, :total_inventory)
+  end
+
 end
