65d7a63476a6004c3b26192b77c42342c16eba99
marks214 <aimee.oz.x@gmail.com>
2020-12-02 02:53:17 -0800

    added customers model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_104000) do
+ActiveRecord::Schema.define(version: 2020_12_02_104922) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.date "registered_at"
+    t.string "address"
+    t.string "city"
+    t.string "state"
+    t.integer "postal_code"
+    t.string "phone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "rentals", force: :cascade do |t|
     t.date "checkin_date"
