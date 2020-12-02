eb602a1e7dcbffcdccef33b44aa6256636941850
roshni-patel <rpatel01@wesleyan.edu>
2020-12-01 21:18:07 -0600

    Can get videos, customers, show videos, create videos, need to add tests

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,2 +1,38 @@
 class VideosController < ApplicationController
+  def index
+    videos = Video.all.as_json(only: [:id, :title, :release_date, :available_inventory])
+    render json: videos, status: :ok
+  end
+
+  def show
+    video = Video.find_by(id: params[:id])
+
+    if video.nil?
+      render json: {ok: false, message: "Not Found"}, status: :not_found
+      return
+    end
+
+    render json: video.as_json(only: [:id, :title, :release_date, :available_inventory]), status: :ok
+  end
+
+  def create
+    video = Video.new(video_params)
+
+    if video.save
+      render json: video.as_json(only: [:id, :title, :release_date, :available_inventory]), status: :created
+      return
+    else
+      render json: {
+          ok: false,
+          errors: video.errors.messages
+      }, status: :bad_request
+      return
+    end
+  end
+
+  private
+
+  def video_params
+    return params.require(:video).permit(:title, :overview, :release_date, :available_inventory, :total_inventory)
+  end
 end
