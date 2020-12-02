a49d3a64f50752802705db8bab8f8be2421a02b6
Genevieve Neely <genevieve.hood@gmail.com>
2020-12-01 18:17:12 -0800

    video model validation tests

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,7 @@
 class Video < ApplicationRecord
   has_many :rentals
+
+  validates :title, :overview, :release_date, presence: true
+  validates :total_inventory, numericality: { :only_integer => true, greater_than_or_equal_to: 0 }
+  validates :available_inventory, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: :total_inventory }
 end
