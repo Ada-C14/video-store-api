8eaa44c36a455443e72a361b9787b316d284cc33
Pauline Chane <pauline.chane@gmail.com>
2020-12-01 20:21:10 -0800

    working validations and model validation tests

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,15 +2,23 @@ class Video < ApplicationRecord
   has_many :customers, through: :rentals
   has_many :rentals
 
-  validates :title, presence: true,
-                    uniqueness: true
-  validates :overview, presence: true
-  validates :release_date, presence: true
-  validates :total_inventory, presence: true,
-                              numericality: {only_integer: true,
+  validate :valid_date
+
+  validates_presence_of :title, :overview, :release_date, :total_inventory, :available_inventory
+  validates :title, uniqueness: true
+  validates :total_inventory, numericality: {only_integer: true,
                                              greater_than: 0}
-  validates :available_inventory, presence: true,
-                                  numericality: {only_integer: true,
+  validates :available_inventory, numericality: {only_integer: true,
                                                  greater_than_or_equal_to: 0,
                                                  less_than_or_equal_to: :total_inventory}
+
+  def valid_date
+    begin
+      unless release_date.is_a?(Date)
+        Date.parse(release_date)
+      end
+    rescue
+      errors.add(:release_date, "must be valid date")
+    end
+  end
 end
--- test/models/video_test.rb
+++ test/models/video_test.rb
@@ -1,7 +1,73 @@
 require "test_helper"
 
 describe Video do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "validations" do
+    it "must consider videos with valid fields valid" do
+      # really a sanity check
+
+      videos.each do |video|
+        expect(video.valid?).must_equal true
+        expect(video.errors).must_be_empty
+      end
+
+    end
+    it "must contain the required fields" do
+      empty_video = Video.create
+
+      expect(empty_video.valid?).must_equal false
+
+      [:title, :release_date, :available_inventory, :overview, :total_inventory].each do |field|
+        expect(empty_video.errors[field]).must_include "can't be blank"
+      end
+
+    end
+    it "must have a unique title" do
+      videos(:black_widow).title = "Wonder Woman 2"
+
+      expect(videos(:black_widow).valid?).must_equal false
+
+      expect(videos(:black_widow).errors[:title]).must_include "has already been taken"
+    end
+
+    it "must have numerical values for total/available inventory" do
+      videos.each do |video|
+        video.total_inventory = "NaN"
+        video.available_inventory = "NaN"
+
+        expect(video.valid?).must_equal false
+
+        expect(video.errors[:available_inventory]).must_include "is not a number"
+        expect(video.errors[:total_inventory]).must_include "is not a number"
+      end
+    end
+
+    it "must have a total_inventory greater than 0" do
+      videos(:black_widow).total_inventory = -1
+      videos(:wonder_woman).total_inventory = 0
+
+      videos.each do |video|
+        expect(video.valid?).must_equal false
+        expect(video.errors[:total_inventory]).must_include "must be greater than 0"
+      end
+    end
+
+    it "must have available inventory between 0 and total_inventory, inclusive" do
+      [-1, videos(:black_widow).total_inventory + 1].each do |stock|
+        videos(:black_widow).available_inventory = stock
+        expect(videos(:black_widow).valid?).must_equal false
+        if stock < 0
+          expect(videos(:black_widow).errors[:available_inventory]).must_include "must be greater than or equal to 0"
+        else
+          expect(videos(:black_widow).errors[:available_inventory]).must_include "must be less than or equal to #{videos(:black_widow).total_inventory}"
+        end
+      end
+    end
+
+    it "must have a release date string carrying a valid date" do
+      videos(:black_widow).release_date = "not a date"
+      expect(videos(:black_widow).valid?).must_equal false
+
+      expect(videos(:black_widow).errors[:release_date]).must_include "must be valid date"
+    end
+  end
 end
