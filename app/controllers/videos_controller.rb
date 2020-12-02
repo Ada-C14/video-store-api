a57fab85e619601dc71626c71bb5bb87d126655b
Genevieve Neely <genevieve.hood@gmail.com>
2020-12-01 17:04:48 -0800

    implement Videos#show controller action

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -6,6 +6,13 @@ class VideosController < ApplicationController
   end
 
   def show
+    video = Video.find_by(id: params[:id])
+    if video
+      render json: video.as_json(only: [:title, :overview, :release_date, :total_inventory, :available_inventory]),
+             status: :ok
+    else
+      render json: { errors: "Not Found" }, status: :not_found
+    end
   end
 
   def create
