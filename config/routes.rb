c7afce329eb5c645cc29bf66f715874e20ebb986
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2020-12-02 15:59:28 -0800

    routes for customer and rental

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'videos/index'
-  get 'videos/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # get 'videos/index'
+  # get 'videos/show'
+
+  resources :customers, only [:index, :show]
+
+
 end
