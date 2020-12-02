7b6bd56601fbcc990f464090ac1b626f66949a66
christinaminh <christina.minh@gmail.com>
2020-12-01 18:01:24 -0800

    Added CustomersController index action and tests. Removed generated routes for customer and replaced with resources for only index and show

--- config/routes.rb
+++ config/routes.rb
@@ -4,13 +4,12 @@ Rails.application.routes.draw do
   get 'videos/index'
   get 'videos/show'
   get 'videos/create'
-  get 'customers/index'
-  get 'customers/show'
   get 'video/index'
   get 'video/show'
   get 'video/create'
-  get 'customer/index'
-  get 'customer/show'
+
+
+  resources 'customers', only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/zomg', to: 'application#test'
 end
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,14 +1,41 @@
 require "test_helper"
 
 describe CustomersController do
-  it "must get index" do
-    get customers_index_url
-    must_respond_with :success
-  end
+  let(:required_customer_attrs) {
+    ["id", "name", "registered_at", "postal_code", "phone", "videos_checked_out_count"]
+  }
 
-  it "must get show" do
-    get customers_show_url
-    must_respond_with :success
+  describe "index" do
+    it "responds with JSON and ok" do
+      get customers_path
+
+      expect(response.header["Content-Type"]).must_include "json"
+      must_respond_with :success
+    end
+
+    it "returns an array of customer hashes" do
+      get customers_path
+
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_instance_of Array
+
+      body.each do |customer|
+        expect(customer).must_be_instance_of Hash
+        expect(customer.keys.sort).must_equal required_customer_attrs.sort
+      end
+    end
+
+    it "will respond with an empty array when there are no customers" do
+      Customer.destroy_all
+
+      get customers_path
+
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_instance_of Array
+      expect(body).must_equal []
+    end
   end
 
 end
