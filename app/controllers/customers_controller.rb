7cf7acd6f9c3dceac857490243d2b0a0e239cc1a
Tram Bui <pbui17@cmc.edu>
2020-12-02 10:22:21 -0600

    routes for customer index and controller action, 2 tests are passed

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,8 @@
 class CustomersController < ApplicationController
 
   def index
-
+    customers = Customer.all.as_json(only: [:id, :name, :phone, :postal_code, :registered_at, :videos_checked_out_count])
+    render json: customers, status: :ok
   end
 
   def show
