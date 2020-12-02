56370ccf1e0b0d441a9f322fa6b3f19e0ae27c06
Ringo Alcock <ringo.alcock@gmail.com>
2020-12-01 18:29:16 -0800

    add strong params for video

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -5,4 +5,10 @@ class VideosController < ApplicationController
     render json: videos.as_json(only: [:id, :title, :release_date, :available_inventory]),
            status: :ok
   end
+
+  private
+
+  def video_params
+    return params.require(:video).permit(:title, :overview, :release_date, :total_inventory, :available_inventory)
+  end
 end
