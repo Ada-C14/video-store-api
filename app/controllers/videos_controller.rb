cdcbaf0188653c7e116270b5fb7ea297196f1f3d
Tram Bui <pbui17@cmc.edu>
2020-12-02 12:53:55 -0600

    passsed the tests for video create

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -8,4 +8,25 @@ class VideosController < ApplicationController
 
   end
 
+  def create
+    video = Video.new(video_params)
+
+    if video.save
+      render json: video.as_json(only: [:id]), status: :created
+      return
+    else
+      render json: {
+          ok: false,
+          errors: video.errors.messages
+      }, status: :bad_request
+      return
+    end
+  end
+
+  private
+
+  def video_params
+    return params.require(:video).permit(:title, :overview, :release_date, :total_inventory, :available_inventory)
+  end
+
 end
