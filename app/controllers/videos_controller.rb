5f67dd74f4f01d566a3f684e4ccb23dc18189f87
Yingjing Lin <yjlin789@gmail.com>
2020-12-01 22:05:17 -0800

    create create method and pass the test

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,4 +3,24 @@ class VideosController < ApplicationController
     videos = Video.all.order(:title)
     render json: videos.as_json(only: ["id", "title", "release_date", "available_inventory"]), status: :ok
   end
+
+  def create
+    video = Video.new(video_params)
+    if video.save
+      render json: {id: video.id}, status: :created
+    else
+      render json: {ok: false,
+                    errors: video.errors.messages},
+             status: :bad_request
+    end
+    return
+  end
+
+
+
+  private
+  def video_params
+    p params
+    return params.require(:video).permit(:id, :title, :release_date, :available_inventory, :overview, :total_inventory)
+  end
 end
