55dacd79624b52edb8698ef09a2d37a13400dd02
Iris Lux <iris.lux0@gmail.com>
2020-12-01 22:37:55 -0800

    show and index for videoscontroller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,2 +1,14 @@
 class VideosController < ApplicationController
+
+  def index
+    videos = Video.all.as_json(only: [:available_inventory, :id, :release_date, :title])
+    render json: videos, status: :ok
+  end
+
+  def show
+    video = Video.find_by(id: params[:id])
+
+    render json: {ok: false, errors:["Not Found"]}, status: :not_found and return if video.nil?
+    render json: video.as_json(except: [:created_at, :updated_at, :id]), status: :ok
+  end
 end
