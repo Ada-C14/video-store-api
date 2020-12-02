e43b58be071333c8f3eb7344c43b1c3a1e775713
Ida <ida.goitom@yahoo.com>
2020-12-02 15:55:25 -0800

    added create action and updated index action and create tests. index and create tests are now passing

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,15 +2,34 @@ class VideosController < ApplicationController
   def index
     videos = Video.all.order(:title)
 
-    render json: videos.as_json(only: [:id, :title, :overview, :release_date, :total_inventory,
-                                     :available_inventory]),
+    render json: videos.as_json(only: [:id, :title, :release_date,
+                                       :available_inventory]),
            status: :ok
+  end
+
+  def create
+    video = Video.new(video_params)
+    if video.save
+      render json: video.as_json(only: [:id]), status: :created
+    else
+      render json: {
+          ok: false,
+          errors: video.errors.messages
+      }, status: :bad_request
+      return
+    end
   end
 
   def zomg
     render json: { ok: 'it works' }, status: :ok
-
   end
 
+  private
+
+  def video_params
+    return params.require(:video).permit(:id, :title, :release_date,
+                                         :available_inventory)
+
+  end
 
 end
