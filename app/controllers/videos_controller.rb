08f392eadc38880d3b308a4b351eaebaaa9a37dc
Ringo Alcock <ringo.alcock@gmail.com>
2020-12-01 19:30:42 -0800

    add videos create method

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -6,6 +6,21 @@ class VideosController < ApplicationController
            status: :ok
   end
 
+  def create
+    video = Video.new(video_params)
+
+    if video.save
+      render json: video.as_json(only: [:id]), status: :created
+      return
+    else
+      render json: {
+          ok: false,
+          errors: video.errors.messages
+      }, status: :bad_request
+      return
+    end
+  end
+
   private
 
   def video_params
--- db/schema.rb
+++ db/schema.rb
@@ -37,4 +37,5 @@ ActiveRecord::Schema.define(version: 2020_12_02_021128) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
+
 end
