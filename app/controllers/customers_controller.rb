b124ae0d4992af9b8091ac08d642e54971b13bac
Kalkidan Ashenafi <kalkidanashenafi@gmail.com>
2020-12-02 09:26:27 -0800

    customer index action added

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,4 +1,9 @@
 class CustomersController < ApplicationController
   def index
+    customers = Customer.all.order(:name)
+
+    render json: customers.as_json(only: [:id, :name, :registered_at, :address, :city,
+                                       :state, :postal_code, :phone]),
+           status: :ok
   end
 end
