0b93d22942df572c30c9dce15daa602bf127441d
Sophie Messing <sophie.e.messing@gmail.com>
2020-12-01 16:20:06 -0800

    created customers_controller.rb and customer.rb model

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,26 @@
+class CustomersController < ApplicationController
+  before_action :set_customer, only: [:show, :update, :destroy]
+
+  # GET /customers
+  def index
+    @customers = Customer.all
+
+    render json: @customers
+  end
+
+  # GET /customers/1
+  def show
+    render json: @customer
+  end
+
+  # POST /customers
+  def create
+    @customer = Customer.new(customer_params)
+
+    if @customer.save
+      render json: @customer, status: :created, location: @customer
+    else
+      render json: @customer.errors, status: :unprocessable_entity
+    end
+  end
+end
--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -1,36 +1,69 @@
+
 require "test_helper"
 
 describe CustomersController do
-  it "must get index" do
-    # Act
-    get customers_path
-    body = JSON.parse(response.body)
+  let(:customer) { customers(:one) }
+  describe "index" do
+    it "must get index" do
+      # Act
+      get customers_path
+      body = JSON.parse(response.body)
 
-    # Assert
-    expect(body).must_be_instance_of Array
-    expect(body.length).must_equal Customer.count
+      # Assert
+      expect(body).must_be_instance_of Array
+      expect(body.length).must_equal Customer.count
 
-    # Check that each customer has the proper keys
-    fields = ["id", "name", "registered_at", "postal_code", 
-      "phone", "videos_checked_out_count"].sort
+      # Check that each customer has the proper keys
+      fields = ["id", "name", "registered_at", "postal_code",
+                "phone", "videos_checked_out_count"].sort
 
-    body.each do |customer|
-      expect(customer.keys.sort).must_equal fields
+      body.each do |customer|
+        expect(customer.keys.sort).must_equal fields
+      end
+
+      must_respond_with :ok
     end
 
-    must_respond_with :ok
+    it "index works even with no customers" do
+      Customer.destroy_all
+
+      get customers_path
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_instance_of Array
+      expect(body.length).must_equal 0
+
+      must_respond_with :ok
+    end
   end
 
-  it "works even with no customers" do
-    Customer.destroy_all
+  # Rails scaffold generated tests below
 
-    get customers_path
-    body = JSON.parse(response.body)
 
-    expect(body).must_be_instance_of Array
-    expect(body.length).must_equal 0
+  it "should create customer" do
+    value do
+      post customers_url, params: { customer: { create: @customer.create, index: @customer.index, show: @customer.show } }, as: :json
+    end.must_differ "Customer.count"
 
-    must_respond_with :ok
+    must_respond_with 201
   end
 
+  it "should show customer" do
+    get customer_url(@customer), as: :json
+    must_respond_with :success
+  end
+
+  it "should update customer" do
+    patch customer_url(@customer), params: { customer: { create: @customer.create, index: @customer.index, show: @customer.show } }, as: :json
+    must_respond_with 200
+  end
+
+  it "should destroy customer" do
+    value do
+      delete customer_url(@customer), as: :json
+    end.must_differ "Customer.count", -1
+
+    must_respond_with 204
+  end
 end
+
