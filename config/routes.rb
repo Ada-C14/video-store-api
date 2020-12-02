a0a2492c08fb36f77d8f44cce0de1b603a483668
Beatrice T <be@trice.codes>
2020-12-02 10:32:23 -0800

    Wrote test for videos#index and updated routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,8 @@
 Rails.application.routes.draw do
   get 'rentals/create'
   get 'rentals/destroy'
-  get 'videos/index'
-  get 'videos/show'
-  get 'videos/create'
-  get 'video/index'
-  get 'video/show'
-  get 'video/create'
 
-
+  resources 'videos', only: [:index, :show, :create]
   resources 'customers', only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/zomg', to: 'application#test'
--- test/controllers/videos_controller_test.rb
+++ test/controllers/videos_controller_test.rb
@@ -6,6 +6,20 @@ describe VideosController do
     must_respond_with :success
   end
 
+  it 'responds with an array of video hashes' do
+    get v
+
+    body = JSON.parse(response.body)
+
+    expect(body).must_be_instance_of Array
+    body.each do |video|
+      expect(video).must_be_instance_of Hash
+      required_video_attrs = ['id', 'title', 'release_date', 'available_inventory']
+      expect(video.keys.sort).must_equal required_video_attrs.sort
+    end
+
+  end
+
   it "must get show" do
     get videos_show_url
     must_respond_with :success
