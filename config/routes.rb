3ccc1819a90abd3c5db5e5c11e21916f7740c710
marks214 <aimee.oz.x@gmail.com>
2020-12-02 03:00:02 -0800

    generated cutomer, rental and video controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'customers/index'
+  get 'videos/index'
+  get 'videos/show'
+  get 'videos/create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/zomg', to: 'application#index'
 
