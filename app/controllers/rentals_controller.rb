55164c799764244de1873f51d7e4a4556850a7e1
Genevieve Neely <genevieve.hood@gmail.com>
2020-12-02 15:24:36 -0800

    rentals controller tests

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,4 +1,7 @@
 class RentalsController < ApplicationController
 
-
+  def checkout
+    customer = Customer.find_by(id: params[:customer_id])
+    video = Video.find_by(id: params[:video_id])
+  end
 end
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,7 +1,64 @@
 require "test_helper"
 
 describe RentalsController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe 'checkout' do
+    before do
+      @customer = customers(:customer_one)
+      @video = videos(:wonder_woman)
+      @rental_hash = {
+          customer_id: @customer.id,
+          video_id: @video.id
+      }
+    end
+    it 'checks out a video to a customer' do
+
+      customer_rental_count = @customer.videos_checked_out_count
+      video_avail_inventory = @video.available_inventory
+
+      expect {
+        post rentals_checkout_path, params: @rental_hash
+      }.must_change "Rental.count"
+
+      rental = Rental.find_by(customer_id: customer.id)
+      @customer.reload
+      @video.reload
+
+      expect(@customer.videos_checked_out_count).must_equal customer_rental_count + 1
+      expect(@video.available_inventory).must_equal video_avail_inventory - 1
+      expect(rental.due_date).must_equal (Date.today + 7).to_s
+
+      must_respond_with :success
+    end
+
+    it 'responds with not found if the customer does not exist' do
+      @rental_hash[:customer_id] = -1
+
+      expect {
+        post rentals_checkout_path, params: @rental_hash
+      }.wont_change "Rental.count"
+
+      must_respond_with :not_found
+    end
+
+    it 'responds with not found if the video does not exist' do
+      @rental_hash[:video_id] = -1
+
+      expect {
+        post rentals_checkout_path, params: @rental_hash
+      }.wont_change "Rental.count"
+
+      must_respond_with :not_found
+    end
+
+    it 'responds with bad request if the video does not have any available inventory' do
+      @video.available_inventory = 0
+
+      expect {
+        post rentals_checkout_path, params: @rental_hash
+      }.wont_change "Rental.count"
+
+      must_respond_with :bad_request
+    end
+
+  end
 end
