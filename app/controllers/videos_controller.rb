6a318e682684c67ce9fe98b404397a82c81c5a40
Pauline Chane <pauline.chane@gmail.com>
2020-12-01 21:01:24 -0800

    show tests failing, need to convert release date back to string

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,8 +1,23 @@
 class VideosController < ApplicationController
   def index
+    videos = Video.all.as_json(only: [:id,
+                                      :title,
+                                      :release_date,
+                                      :available_inventory])
+    render json: videos, status: :ok
   end
 
   def show
+    begin
+      video = Video.find_by_id(params[:id]).as_json(only: [:title,
+                                                           :overview,
+                                                           :release_date,
+                                                           :total_inventory,
+                                                           :available_inventory])
+      render json: video, status: :ok
+    rescue
+      render json: {"errors": ["Not Found"]}, status: :not_found
+    end
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'rentals/create'
-  get 'videos/index'
-  get 'videos/show'
+  resources :videos, only: [:index, :show, :create]
   get 'customers/index'
   get 'customers/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
