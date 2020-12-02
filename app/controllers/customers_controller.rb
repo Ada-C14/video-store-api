0ec16927db7989b132f1ced4bfcd9ee6f553a772
Emily Nagai <emnagai89@gmail.com>
2020-12-02 14:47:16 -0800

    implemented show and included in routes

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -5,6 +5,17 @@ class CustomersController < ApplicationController
     render json: customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :videos_checked_out_count]), status: :ok
   end
 
+  def show
+    customer = Customer.find_by(id: params[:id])
+
+    if customer.nil?
+      render json: { ok: false, message: "Not Found"}, status: :not_found
+      return
+    end
+
+    render json: customer.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :videos_checked_out_count]), status: :ok
+  end
+
 
 
 end
