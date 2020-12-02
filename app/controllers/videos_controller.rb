3e44203a7ffbcef4c4236d327a535eefabee619b
Alice Boone <alicegbonel@gmail.com>
2020-12-01 22:44:27 -0800

    Add action show to the controller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,7 +1,25 @@
 class VideosController < ApplicationController
+
   def index
     videos = Video.all.order(:title)
-    render json: videos.as_json(only: ["id", "title", "release_date", "available_inventory"]), status: :ok
+    render json: videos.as_json(only: ["id", "title", "release_date", "available_inventory"]),
+                                status: :ok
+  end
+
+  def show
+    video = Video.find_by(id: params[:id])
+
+    if video.nil?
+      render json: {
+          ok: false,
+          message: "Not found"
+      }, status: :not_found
+
+      return
+    end
+
+    render json: video.as_json(only: ["available_inventory", "overview", "release_date", "title", "total_inventory"]),
+                                status: :ok
   end
 
   def create
@@ -17,8 +35,8 @@ class VideosController < ApplicationController
   end
 
 
-
   private
+
   def video_params
     p params
     return params.require(:video).permit(:id, :title, :release_date, :available_inventory, :overview, :total_inventory)
