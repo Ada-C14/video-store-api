2133f46d4d8514c35879a1ee8a8a196355512db4
r-spiel <richelle.mehlhaff@gmail.com>
2020-12-02 15:49:41 -0800

    added relationship between rentals and customer/videos

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,7 +1,10 @@
 class Video < ApplicationRecord
+  has_many :rentals
+
   validates :title, presence: true
   validates :overview, presence: true
   validates :release_date, presence: true
   validates :total_inventory, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :available_inventory, presence: true, numericality: { only_integer:  true, greater_than_or_equal_to: 0 }
+
 end
