acf851766f58ec463abcf2bab74109edce1c09c8
Emily Nagai <emnagai89@gmail.com>
2020-12-02 14:48:02 -0800

    show postive nominal test

--- test/controllers/customers_controller_test.rb
+++ test/controllers/customers_controller_test.rb
@@ -14,28 +14,39 @@ describe CustomersController do
       expect(body).must_be_instance_of Array
       expect(body.length).must_equal Customer.count
 
-      # Check that each customer has the proper keys
-      fields = ["id", "name", "registered_at", "postal_code",
-                "phone", "videos_checked_out_count"].sort
-
       body.each do |customer|
-        expect(customer.keys.sort).must_equal fields
+        expect(customer.keys.sort).must_equal FIELDS
       end
 
       must_respond_with :ok
     end
 
-    # it "works even with no customers" do
-    #   Customer.destroy_all
-    #
-    #   get customers_path
-    #   body = JSON.parse(response.body)
-    #
-    #   expect(body).must_be_instance_of Array
-    #   expect(body.length).must_equal 0
-    #
-    #   must_respond_with :ok
-    # end
+    it "works even with no customers" do
+      Customer.destroy_all
+
+      get customers_path
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_instance_of Array
+      expect(body.length).must_equal 0
+
+      must_respond_with :ok
+    end
+  end
+
+  describe "show" do
+    it "will return back a hash with the proper fields" do
+      customer = customers(:customer_one)
+
+      get customer_path(customer.id)
+
+      body = JSON.parse(response.body)
+
+      must_respond_with :ok
+      expect(body).must_be_instance_of Hash
+      expect(response.header['Content-Type']).must_include 'json'
+      expect(body.keys.sort).must_equal FIELDS
+    end
   end
 
 
