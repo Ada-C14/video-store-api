eed26d0ef86bb45dc066a17eaf46f79edcc749ae
christinaminh <christina.minh@gmail.com>
2020-12-02 16:44:20 -0800

    Started Rental create action and added tests

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,7 +1,24 @@
 class RentalsController < ApplicationController
   def create
+    rental = Rental.new(rental_params)
+
+    if rental.save
+      render json: {
+        customer_id: rental.customer_id,
+        video_id: rental.video_id,
+        due_date: rental.due_date,
+        videos_checked_out_count: rental.customer.videos_checked_out_count,
+        available_inventory: rental.video.available_inventory
+      }, status: :created
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def rental_params
+    return params.require(:rental).permit(:customer_id, :video_id)
   end
 end
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,14 +1,42 @@
 require "test_helper"
 
 describe RentalsController do
-  it "must get create" do
-    get rentals_create_url
-    must_respond_with :success
+  let(:customer) { customers(:customer_one) }
+  let(:video) { videos(:wonder_woman) }
+
+  let(:rental_data) {
+    {
+      rental: {
+        customer_id: customer.id,
+        video_id: video.id,
+      }
+    }
+  }
+
+  describe "create/checkout" do
+    it "creates a new rental" do
+      expect{
+        post check_out_path, params: rental_data
+      }.must_change "Rental.count", 1
+
+      must_respond_with :created
+    end
+
+    it "increase the customer's videos_checked_out_count by one" do
+      expect{
+        post check_out_path, params: rental_data
+      }.must_change "Customer.videos_checked_out_count.count", 1
+    end
+
+    it "decrease the video's available_inventory by one" do
+      expect{
+        post check_out_path, params: rental_data
+      }.must_change "Video.available_inventory.count", -1
+    end
   end
 
+
   it "must get destroy" do
-    get rentals_destroy_url
-    must_respond_with :success
   end
 
 end
