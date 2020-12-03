d3086fe16f522367ebb829defeeb82e3fbe8ee26
Blaine <b.atsbeha@gmail.com>
2020-12-03 14:54:58 -0800

    created checkin route

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,6 @@ Rails.application.routes.draw do
   resources :videos, only: [:index, :show, :create]
 
   post '/rentals/check-out', to: 'rentals#checkout', as: 'checkout'
-
+  post '/rentals/check-in', to: 'rentals#checkin', as: 'checkin'
+  
 end
