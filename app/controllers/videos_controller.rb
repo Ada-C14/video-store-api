28d09962b30d2f2109004f70b6611772f240d91e
Jessica Chan <jwinchan@gmail.com>
2020-12-03 02:05:14 -0800

    added videos show method

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -6,4 +6,19 @@ class VideosController < ApplicationController
     render json: videos.as_json(only: [:id, :title, :release_date, :available_inventory]),
                                 status: :ok
   end
+
+  def show
+    video = Video.find_by(id: params[:id])
+
+    if video.nil?
+      render json: {
+          ok: false,
+          errors: "Not Found"
+      }, status: :not_found
+      return
+    end
+
+    render json: video.as_json(only: [:title, :overview, :release_date, :total_inventory, :available_inventory]),
+                              status: :ok
+  end
 end
