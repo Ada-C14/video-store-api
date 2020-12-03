fd8b641073d2adcdd29e5253f85febb443b06aae
KayKay-git <kayalew@yahoo.com>
2020-12-02 19:02:53 -0500

    Added check-in , checkout and due date columns. Also added action methods for rentals controller

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,4 +1,17 @@
 class RentalsController < ApplicationController
-  def create
+  # def create
+  # end
+
+  def check_out
+
+  end
+
+  def check_in
+
+  end
+
+
+  def rental_params
+    params.require(:rental).permit(:customer_id, :video_id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_013050) do
+ActiveRecord::Schema.define(version: 2020_12_02_235052) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,9 @@ ActiveRecord::Schema.define(version: 2020_12_02_013050) do
     t.integer "video_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.date "due_date"
+    t.date "check_out"
+    t.date "check_in"
   end
 
   create_table "videos", force: :cascade do |t|
@@ -42,7 +45,7 @@ ActiveRecord::Schema.define(version: 2020_12_02_013050) do
     t.integer "available_inventory"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "release_date"
+    t.date "release_date"
   end
 
 end
