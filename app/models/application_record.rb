7d1fb6fa2ac6f39fdde2a62e53208c8826bfa662
Emily Nagai <emnagai89@gmail.com>
2020-12-03 09:23:31 -0800

    checkout checkin methods

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,21 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def check_out
+    if self.class.name == "Video"
+      self.available_inventory -= 1
+    else
+      self.videos_checked_out_count += 1
+    end
+    self.save
+  end
+
+  def check_in
+    if self.class.name == "Video"
+      self.available_inventory += 1
+    else
+      self.videos_checked_out_count -= 1
+    end
+    self.save
+  end
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -10,4 +10,6 @@ class Customer < ApplicationRecord
 
   has_many :videos, through: :rentals
 
+
+
 end
