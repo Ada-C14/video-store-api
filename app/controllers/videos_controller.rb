e93617f0766e0c91025cec882f09893880e0d066
Ana <anakp07@gmail. omgit config --global user.email anakp07@gmail.com>
2020-12-02 17:10:28 -0800

    created show and create method for Video Controller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -5,9 +5,25 @@ class VideosController < ApplicationController
   end
 
   def show
+    video = Video.find_by(id: params[:id])
+
+    if video.nil?
+      render json: {
+          ok: false,
+          message: 'not found',
+      }, status: :not_found
+      return
+    end
+    render json:video.as_json(only: [:title, :overview, :release_date, :total_inventory, :available_inventory]), status: :ok
   end
 
   def create
+    video = Video.new(video_params)
+    if video.save
+      render json:video.as_json, status: :created
+    else
+      #
+    end
   end
 end
 
