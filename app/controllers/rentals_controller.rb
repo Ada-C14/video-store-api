26a23f7457643588bdb1cc2a97334e4a0a8dd490
Li Dai <li.lea.dai@gmail.com>
2020-12-03 12:45:02 -0800

    Get errors when checking out and invalid video/customers

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -3,33 +3,29 @@ class RentalsController < ApplicationController
 
     customer = Customer.find_by(id: params[:customer_id])
     video = Video.find_by(id: params[:video_id])
-    # if customer.nil?
-    # elsif video.nil?
-    # end
-
-
-    rental = Rental.new(customer_id: customer.id, video_id: video.id, due_date: Date.today + 1.week)
-    if rental.save
-      customer.increase_checkedout_videos
-      video.decrease_available_inventory
-      render json: {customer_id: customer.id,
-                    video_id: video.id,
-                    due_date: Date.today + 1.week,
-                    videos_checked_out_count:customer.videos_checked_out_count,
-                    available_inventory:video.available_inventory},
-             status: :ok
-    else
+    if customer.nil? || video.nil?
       render json: {
-          #    ok: false,
-          errors: rental.errors.messages
-      }, status: :bad_request
-      return
+          errors: ['Not Found']
+      }, status: :not_found
+    else
+      rental = Rental.new(customer_id: customer.id, video_id: video.id, due_date: Date.today + 1.week)
+      if rental.save
+        customer.increase_checkedout_videos
+        video.decrease_available_inventory
+        render json: {customer_id: customer.id,
+                      video_id: video.id,
+                      due_date: Date.today + 1.week,
+                      videos_checked_out_count:customer.videos_checked_out_count,
+                      available_inventory:video.available_inventory},
+               status: :ok
+      else
+        render json: {
+            #    ok: false,
+            errors: rental.errors.messages
+        }, status: :bad_request
+        return
+      end
     end
-
-
-
-    # due_date = rental.created_at.strftime("%F") + 1.week
-
 
 
 
