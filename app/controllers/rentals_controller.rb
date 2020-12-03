149dcd105614d81221a9f3af7b7287896107b413
r-spiel <richelle.mehlhaff@gmail.com>
2020-12-02 20:57:42 -0800

    update rental controller and controller tests for error messaging

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -3,6 +3,17 @@ class RentalsController < ApplicationController
   def checkout
     video = Video.find_by(id: params[:video_id])
     customer = Customer.find_by(id: params[:customer_id])
+
+    if video.nil?
+      render json: { errors: ["Not Found"] }, status: :not_found
+      return
+    end
+
+    if customer.nil?
+      render json: { errors: ["Not Found"] }, status: :not_found
+      return
+    end
+
     rental = Rental.new(
         due_date: (Date.today + 7),
         customer_id: customer.id,
@@ -17,10 +28,10 @@ class RentalsController < ApplicationController
       # # call method to decrease available inventory
       rental.available_inventory = video.toggle_down_inventory
       render json: rental.as_json(only: [:customer_id, :video_id, :due_date, :videos_checked_out_count, :available_inventory]),
-             status: :created
+             status: :ok
       return
     else
-      render json: { errors: rental.errors.messages }, status: :bad_request
+      render json: { errors: rental.errors.messages }, status: :not_found
       return
     end
 
