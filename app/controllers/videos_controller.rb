112d51e784227e44727e0cc22d9e108f003bce91
Stacy <lundquist.stacy@gmail.com>
2020-12-03 15:09:30 -0800

    fixed postman test

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -26,7 +26,6 @@ class VideosController < ApplicationController
       return
     else
       render json: {
-          ok: false,
           errors: video.errors.messages
       }, status: :bad_request
     end
