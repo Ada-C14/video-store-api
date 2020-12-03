f4162f3b19a856f021facbc72eb9a6df4c547e9b
Marj E <marj@marjorieetienne.com>
2020-12-02 16:52:28 -0800

    added video create method and strong params to controller

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -18,4 +18,25 @@ class VideosController < ApplicationController
 
     render json: video.as_json(only: [:overview, :title, :release_date, :available_inventory, :total_inventory]),  status: :ok
   end
+
+  def create
+    video = Video.new(video_params)
+
+    if video.save
+      render json: video.as_json(only: [:id]), status: :created
+      return
+    else
+      render json: {
+          ok: false,
+          errors: video.errors.messages
+      }, status: :bad_request
+      return
+    end
+  end
+
+  private
+
+  def video_params
+    return params.require(:video).permit(:id, :title, :release_date, :available_inventory, :overview,:total_inventory)
+  end
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,9 +1,4 @@
 class Video < ApplicationRecord
   has_many :rentals
   has_many :customers, through: :rentals
-
-  def format_date(date)
-    date.strftime("%B")
-  end
-
 end
