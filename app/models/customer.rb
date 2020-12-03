63edb158177ed03711ec7da246cc3550c50ef129
Ana <anakp07@gmail. omgit config --global user.email anakp07@gmail.com>
2020-12-02 20:26:41 -0800

    added model validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,11 @@
 class Customer < ApplicationRecord
+  has_many :rentals
+  # has_many :videos, through: :rentals
+
+  validates :name, presence: true, uniqueness: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :postal_code, presence: true
+  validates :phone, presence: true, uniqueness: true
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,10 @@
 class Video < ApplicationRecord
+  has_many :rentals
+  # has_many :customers, through: :rentals
+
+  validates :title, presence: true, uniqueness: true
+  validates :overview, presence: true
+  validates :release_date, presence: true
+  validates :total_inventory, presence: true
+  validates :available_inventory, presence: true
 end
