50f8d18101e0704f96f2a82625e43f024e17d759
Tram Bui <pbui17@cmc.edu>
2020-12-02 21:35:04 -0600

    added relations tests to rental model

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,7 +1,59 @@
 require "test_helper"
 
 describe Rental do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "validations" do
+
+  end
+
+  describe "relations" do
+    describe "videos" do
+      before do
+        @video = videos(:wonder_woman)
+        customer = customers(:customer_one)
+        @rental = Rental.new(customer: customer,
+                             due_date: "2020-12-25")
+      end
+
+      it "can set the video using Video" do
+        @rental.video = @video
+        expect(@rental.video_id).must_equal @video.id
+      end
+
+      it "can set the video using video_id" do
+        @rental.video_id = @video.id
+        expect(@rental.video).must_equal @video
+      end
+
+      it "has a video" do
+        rental = rentals(:rental_one)
+        expect(rental).must_respond_to :video
+        expect(rental.video).must_be_kind_of Video
+      end
+
+    end
+
+    describe "customers" do
+      before do
+        video = videos(:wonder_woman)
+        @customer = customers(:customer_one)
+        @rental = Rental.new(video: video,
+                             due_date: "2020-12-25")
+      end
+      it "has a customer" do
+        rental = rentals(:rental_one)
+        expect(rental).must_respond_to :customer
+        expect(rental.customer).must_be_kind_of Customer
+      end
+
+      it "can set the customer using Customer" do
+        @rental.customer = @customer
+        expect(@rental.customer_id).must_equal @customer.id
+      end
+
+      it "can set the customer using customer_id" do
+        @rental.customer_id = @customer.id
+        expect(@rental.customer).must_equal @customer
+      end
+    end
+  end
 end
