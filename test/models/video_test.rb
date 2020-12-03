77c3ff2e4ce7262997d04e5b433799872fcf1471
Alice D <alicedilanchian@gmail.com>
2020-12-03 10:37:17 -0500

    video  model - relations - has rentals

--- test/models/video_test.rb
+++ test/models/video_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 describe Video do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "relations" do
+
+    it "has rentals" do
+      video_one = customers(:wonder_woman)
+      expect(video_one).must_respond_to :rentals
+      video_one.rentals.each do |rental|
+        expect(rental).must_be_kind_of Rental
+      end
+    end
+
+  end
 end
