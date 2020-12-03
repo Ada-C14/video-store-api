f022019e7dd76d2a1633971372331e76a90bf210
Christabel Sebastian <christabel.escarez@gmail.com>
2020-12-02 17:10:09 -0800

    outlined tests

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,7 +1,15 @@
 require "test_helper"
 
 describe Customer do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "relationships" do
+
+  end
+
+  describe "validations" do
+
+  end
+
+  describe "custom methods" do
+
+  end
 end
--- test/models/video_test.rb
+++ test/models/video_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 describe Video do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "relationships" do
+    it "has many rentals" do
+
+    end
+    it "has many customers through rentals" do
+
+    end
+  end
+
+  describe "validations" do
+    it "must have a title" do
+
+    end
+    it "must have a overview" do
+
+    end
+    it "must have a total_inventory" do
+
+    end
+    it "must have a available_inventory" do
+
+    end
+    it "must have a release_date" do
+
+    end
+  end
+
+  describe "custom methods" do
+
+  end
 end
