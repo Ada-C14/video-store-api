0785efeb7953aaf3501f60deee4949f4ef34ea54
Emily Nagai <emnagai89@gmail.com>
2020-12-03 11:39:15 -0800

    migration to add customer and video refs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_03_165737) do
+ActiveRecord::Schema.define(version: 2020_12_03_193021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,10 @@ ActiveRecord::Schema.define(version: 2020_12_03_165737) do
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "checked_out"
     t.datetime "checked_in"
+    t.bigint "video_id"
+    t.bigint "customer_id"
+    t.index ["customer_id"], name: "index_rentals_on_customer_id"
+    t.index ["video_id"], name: "index_rentals_on_video_id"
   end
 
   create_table "videos", force: :cascade do |t|
