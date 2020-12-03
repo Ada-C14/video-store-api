85874a40ba6edd6018e46577c8fab37bbdbddf6b
Tram Bui <pbui17@cmc.edu>
2020-12-02 20:30:40 -0600

    tried to write check-out action

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,9 +1,22 @@
 class RentalsController < ApplicationController
   def check_out
+    rental = Rental.new(video: @video, customer: @customer ,due_date: Time.now + 7 )
 
   end
 
   def check_in
 
   end
+
+  private
+
+  def find_customer
+    @customer = Customer.find_by(id: params[:id])
+  end
+
+  def find_video
+    @video = Video.find_by(id: params[:id])
+  end
+
+
 end
