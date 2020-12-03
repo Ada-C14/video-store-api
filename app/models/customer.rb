b3ee949dc69b0a77f690a02572e76f62494a4812
Emily Nagai <emnagai89@gmail.com>
2020-12-02 21:10:26 -0800

    included customer validations and began mode validatation tests

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,13 @@
 class Customer < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, uniqueness: true
+  validates :registered_at, presence: true
+  validates :address, presence: true
+  validates :city, presence: true
+  validates :state, presence: true
+  validates :postal_code, presence: true
+  validates :phone, presence: true, uniqueness: true
+  validates :videos_checked_out_count, numericality: true #not sure about this? customer can have 0 checked out
+
+  #has_many :videos, through: rentals?
+
 end
--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 describe Customer do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "validations" do
+    it "validates that all fields are present" do
+      customer = customers(:customer_one)
+
+      expect(customer.valid?).must_equal true
+    end
+
+    it "must include customer's name" do
+      customer = customers(:customer_one)
+      customer.name = nil
+      customer2 = customers(:customer_two)
+
+
+      expect(customer.valid?).must_equal false
+      expect(customer.errors.messages).must_include :name
+      expect(customer2.valid?).must_equal true #does this really work?
+    end
+  end
 end
