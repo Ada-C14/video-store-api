ec5c0ce161e30895b8d26acea04b70f5bd395453
gessica <gessicamohr18@gmail.com>
2020-12-02 17:21:03 -0700

    added index and create to video controller, tests are passing, added validations to video model

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,2 +1,23 @@
 class VideosController < ApplicationController
+
+  def index
+    videos = Video.all.as_json(only: [:id, :title, :release_date, :available_inventory ] )
+    render json: videos, status: :ok
+  end
+
+  def create
+    video = Video.new(video_params)
+
+    if video.save
+      render json: video.as_json(only: [:id]), status: :created
+    else
+      render json: { errors: video.errors.messages }, status: :bad_request
+    end
+  end
+
+  private
+
+  def video_params
+    params.permit(:title, :overview, :release_date, :total_inventory, :available_inventory)
+  end
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,7 @@
 class Video < ApplicationRecord
   has_many :rentals
+
+  validates :title, :overview, :release_date, presence: true
+  validates :total_inventory, :available_inventory, numericality: { :only_integer => true, greater_than_or_equal_to: 0 }
+
 end
