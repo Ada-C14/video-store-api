7b3c73b358621fd9792d4ca9f2e4f093694533c1
Ren Carothers <rencarothers@gmail.com>
2020-12-02 23:07:45 -0800

    adjust response to pass controller tests

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,7 @@
 class CustomersController < ApplicationController
   def index
     customers = Customer.all.order(:name)
-    render json: customers, status: :ok
+    render json: customers.as_json(only: [:id, :name, :phone, :postal_code, :registered_at, :videos_checked_out_count]), status: :ok
   end
 
 end
