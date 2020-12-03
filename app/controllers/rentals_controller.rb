80c65e422d0e3a8893c24cf5f41b945ee0d8d52a
Jasmine <jasylop@gmail.com>
2020-12-02 22:25:55 -0800

    added line to destroy rental

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -2,6 +2,7 @@ class RentalsController < ApplicationController
 
   def check_out
     rental = Rental.new(customer_id: params[:customer_id], video_id: params[:video_id], due_date: Date.today + 7)
+
     if rental.save
       rental.customer.videos_checked_out_count += 1
       rental.customer.save!
@@ -11,8 +12,33 @@ class RentalsController < ApplicationController
         return render json: rental.as_json(only: [:customer_id, :video_id, :due_date]).merge(
           videos_checked_out_count: rental.customer.videos_checked_out_count,
           available_inventory: rental.video.available_inventory
-        )
+        ), status: :ok
       else
+        rental.destroy
+        return render json: { errors: rental.video.errors.messages }, status: :bad_request
+      end
+
+    else
+      return render json: { errors: ["Not Found"] }, status: :not_found
+    end
+  end
+
+
+  def check_out
+    rental = Rental.new(customer_id: params[:customer_id], video_id: params[:video_id], due_date: Date.today + 7)
+
+    if rental.save
+      rental.customer.videos_checked_out_count += 1
+      rental.customer.save!
+
+      rental.video.available_inventory -= 1
+      if rental.video.save
+        return render json: rental.as_json(only: [:customer_id, :video_id, :due_date]).merge(
+            videos_checked_out_count: rental.customer.videos_checked_out_count,
+            available_inventory: rental.video.available_inventory
+        ), status: :ok
+      else
+        rental.destroy # i want to destroy the rental created because video is invalid
         return render json: { errors: rental.video.errors.messages }, status: :bad_request
       end
 
