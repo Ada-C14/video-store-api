7bda7423aec8aaf7c9ac9186634260a7f830a2ff
Kalkidan Ashenafi <kalkidanashenafi@gmail.com>
2020-12-02 17:57:20 -0800

    Customer index test + show for videos

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -2,7 +2,7 @@ class VideosController < ApplicationController
   def index
     videos = Video.all.order(:title)
 
-    render json: videos.as_json(only: [:id, :title, :overview, :release_date, :total_inventory,
+    render json: videos.as_json(only: [:id, :title, :release_date,
                                      :available_inventory]),
            status: :ok
   end
@@ -12,5 +12,20 @@ class VideosController < ApplicationController
 
   end
 
+  def show
+    video = Video.find_by(id: params[:id])
+
+    if video
+      render json: video.as_json( only: [:title, :overview, :release_date, :total_inventory, :available_inventory]),
+             status: :ok
+      return
+    else
+      render json: {
+          ok: false,
+          message: 'Not found'
+      }, status: :not_found
+      return
+    end
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  resources :videos, only: [:index]
-  resources :customers, only: [:index]
+  resources :videos, only: [:index, :show, :create]
+  resources :customers, only: [:index, :show]
+
+  # post "/videos"
   # get "/zomg", to: "videos#zomg"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
