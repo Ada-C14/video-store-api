d855d7162e62d90f7170994e020a1ea861a51c7c
Tram Bui <pbui17@cmc.edu>
2020-12-03 10:52:36 -0600

    worked on check_out controller logic and custom methods

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,6 +1,18 @@
 class RentalsController < ApplicationController
   def check_out
-    rental = Rental.new(video: @video, customer: @customer ,due_date: Time.now + 7 )
+    rental = Rental.new(video: @video, customer: @customer ,due_date: Time.now + 7.days )
+
+    if rental.save
+      # do something
+      rental.decrease_available_inventory
+      rental.increase_videos_checked_out
+
+      render json: rental.as_json(only: [:id, :due_date]), status: :created
+      return
+    else # doesn't save, surface error messages
+      render json: {ok: false, errors: rental.errors.messages}, status: :bad_request
+      return
+    end
 
   end
 
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,14 @@
 class Rental < ApplicationRecord
   belongs_to :customer
   belongs_to :video
+
+  def decrease_available_inventory
+    self.video.available_inventory -= 1
+    self.video.save!
+  end
+
+  def increase_videos_checked_out
+    self.customer.videos_checked_out_count += 1
+    self.customer.save!
+  end
 end
