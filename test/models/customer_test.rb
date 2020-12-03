c03250ce914ce46b2405e1d02b2900c123e32ab4
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2020-12-02 20:25:33 -0800

    customer validation tests all passing

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,28 +1,58 @@
 require "test_helper"
 
 describe Customer do
-  let (:alligator) {pets(:valid_pet)}
-  it "can be created" do
-    expect(alligator.valid?).must_equal true
-  end
+  describe "validations" do
+    let (:customer_one) {customers(:customer_one)}
+    it "can be created" do
+      expect(customer_one.valid?).must_equal true
+    end
 
-  it "requires name, age and human" do
-    required_fields = [:name, :species, :age, :owner]
+    it "requires name address city state postal_code phone" do
+      required_fields = [:name, :address, :city, :state, :postal_code, :phone]
 
-    required_fields.each do |field|
-      alligator[field] = nil
+      required_fields.each do |field|
+        customer_one[field] = nil
 
-      expect(alligator.valid?).must_equal false
+        expect(customer_one.valid?).must_equal false
 
-      alligator.reload
+        customer_one.reload
+      end
+    end
+
+    it "requires a name" do
+      customer_one.name = nil
+
+      expect(customer_one.valid?).must_equal false
+    end
+
+    it "requires an address" do
+      customer_one.name = nil
+
+      expect(customer_one.valid?).must_equal false
+    end
+
+    it "requires a city" do
+      customer_one.name = nil
+
+      expect(customer_one.valid?).must_equal false
+    end
+
+    it "requires a state" do
+      customer_one.name = nil
+
+      expect(customer_one.valid?).must_equal false
+    end
+
+    it "requires a postal_code" do
+      customer_one.name = nil
+
+      expect(customer_one.valid?).must_equal false
+    end
+
+    it "requires a phone" do
+      customer_one.name = nil
+
+      expect(customer_one.valid?).must_equal false
     end
   end
-
-  it "requires a numeric age" do
-    alligator.age = "three"
-
-    expect(alligator.valid?).must_equal false
-  end
-end
-
 end
