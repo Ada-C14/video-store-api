78de93efa401b635acfaa346967f4622d01a7646
Lina Do <l.do5147@gmail.com>
2020-12-03 10:41:22 -0800

    added route for post checkout action

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   resources :customers, only: [:index]
 
   resources :videos, only: [:index, :show, :create]
+
+  post '/rentals/check-out', to: 'rentals#checkout', as: 'checkout'
+
 end
