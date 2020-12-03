ceaf5c793633164b041f1d95b4bc6562efc16b93
Emily Nagai <emnagai89@gmail.com>
2020-12-02 21:42:12 -0800

    test for validation that a name is unique

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -8,15 +8,25 @@ describe Customer do
       expect(customer.valid?).must_equal true
     end
 
-    it "must include customer's name" do
+    it "must include customer's name and fails validation if it's not there" do
       customer = customers(:customer_one)
       customer.name = nil
-      customer2 = customers(:customer_two)
 
+      customer2 = Customer.find_by(name: "Becca")
 
       expect(customer.valid?).must_equal false
       expect(customer.errors.messages).must_include :name
-      expect(customer2.valid?).must_equal true #does this really work?
+      expect(customer.errors.messages[:name]).must_equal ["can't be blank"]
+      expect(customer2.valid?).must_equal true
+    end
+
+    it "fails validation if customer names are not unique" do
+      customer = Customer.find_by(name: "Simon Del Rosario")
+      customer2 = Customer.create(name: "Simon Del Rosario", registered_at: "Thur, 29 Apr 2015 07:54:14 -0700", address: "23424 weeee", city: "Lynnwood", state: "WA", postal_code: "98126", phone: "(123)423-4566", videos_checked_out_count: 3)
+
+      expect(customer2.valid?).must_equal false
+      expect(customer2.errors.messages).must_include :name
+      expect(customer2.errors.messages[:name]).must_equal ["has already been taken"]
     end
   end
 end
