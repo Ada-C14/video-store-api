969f9e6c71272fc9df5bb75bc1d2159772c26d55
Noor B.A <noor.bahr.aluloom@gmail.com>
2020-12-03 11:21:03 -0800

    generated rental controller and model

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,6 @@
 class Video < ApplicationRecord
+  has_many :rentals
+  has_many :customers, through: :rentals
+
   validates :title, :overview, :release_date, :total_inventory, :available_inventory, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_023226) do
+ActiveRecord::Schema.define(version: 2020_12_03_191107) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,11 @@ ActiveRecord::Schema.define(version: 2020_12_02_023226) do
     t.string "postal_code"
     t.string "phone"
     t.integer "videos_checked_out_count"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "rentals", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
