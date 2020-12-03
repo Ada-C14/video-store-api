57dbf05bf9c4a2427ad58b51f8f1a0541781d5c9
Lina Do <l.do5147@gmail.com>
2020-12-03 11:10:31 -0800

    wrote rental create and error tests and checkout controllers

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,32 @@
 class RentalsController < ApplicationController
+
+  def checkout
+
+
+
+
+    rental = Rental.new(rental_params)
+
+    # create a new rental
+    if rental.save
+      # increase the customers video checkout count
+      # decrease video available inventory
+      # create due date - 7 days from current date
+      render json: rental.as_json(only: [:customer_id, :video_id, :due_date]), status: :ok
+      return
+    else
+      # invalid request
+      render json: { errors: rental.errors.messages }, status: :bad_request
+      return
+    end
+
+
+  end
+
+  private
+
+  def rental_params
+    return params.permit(:customer_id, :video_id)
+  end
+
 end
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,7 +1,43 @@
 require "test_helper"
 
 describe RentalsController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "checkout" do
+
+    let(:rental_params) {
+      {
+          customer_id: customers(:customer_one).id,
+          video_id: videos(:wonder_woman).id
+      }
+    }
+
+    it "can create a valid rental" do
+      expect {
+        post checkout_path, params: rental_params
+      }.must_differ "Rental.count", 1
+
+      must_respond_with :ok
+    end
+
+    it "will respond with bad request and errors for an invalid rental" do
+
+      # Arrange
+      rental_params[:customer_id] = nil
+
+      # Assert
+      expect {
+        post checkout_path, params: rental_params
+      }.wont_change "Rental.count"
+      body = JSON.parse(response.body)
+
+      expect(body.keys).must_include "errors"
+      expect(body["errors"].keys).must_include "customer_id"
+      expect(body["errors"]["customer_id"]).must_include "can't be blank"
+
+      must_respond_with :bad_request
+
+    end
+
+
+
+  end
 end
