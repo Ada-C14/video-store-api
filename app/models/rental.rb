63a82eb4dd0ff442eef9b26e150087be84ae5b89
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-12-03 00:00:31 -0800

    added model helper methods for check_out action in rental

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -3,4 +3,8 @@ class Rental < ApplicationRecord
   belongs_to :video
 
   validates :customer_id, :video_id, presence: true
+
+  def checkout_update
+    self.update(due_date: Date.today.advance(days: 7).to_s(:db))
+  end
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -4,4 +4,7 @@ class Video < ApplicationRecord
   validates :title, :overview, :release_date, presence: true
   validates :total_inventory, :available_inventory, numericality: { :only_integer => true, greater_than_or_equal_to: 0 }
 
+  def checkout_update
+    self.update(available_inventory: self.available_inventory - 1)
+  end
 end
