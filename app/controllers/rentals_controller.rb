d0a7cc1664c510783b73b38537fbfed0667ee46a
Emily Nagai <emnagai89@gmail.com>
2020-12-03 09:08:27 -0800

    rental routes and pseudocode

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,10 @@
 class RentalsController < ApplicationController
+  #def create
+  # find video to checkout
+  # create rental
+  #
+  # end
+  #
+  # def checked_in
+  # end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  #
+  post 'rentals/checked_in'
+  resources :rentals, only: [:create]
   resources :customers, only: [:index, :show, :create]
   resources :videos, only: [:index, :show, :create]
+
 end
