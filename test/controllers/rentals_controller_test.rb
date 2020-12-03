9bd433af7bf78bb6f453cfd25e241e945e66187c
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-12-03 00:01:18 -0800

    added tests for check_out action for rental

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -2,47 +2,65 @@ require "test_helper"
 
 describe RentalsController do
   describe "check-out" do
-    it "can check-out a valid video" do
-      # Arrange
-      video_hash = {
-        title: "Alf the movie",
-        overview: "The most early 90s movie of all time",
-        release_date: "December 16th 2025",
-        total_inventory: 6,
-        available_inventory: 6
+    let (:rental_hash) {
+      {     
+        customer_id: customers(:customer_one).id,
+        video_id: videos(:wonder_woman).id,
       }
+    }  
+    it "can check-out a valid rental" do
+      # Arrange
+      before_videos_count = customers(:customer_one).videos_checked_out_count
+      before_inventory = videos(:wonder_woman).available_inventory
+
+      fields = ["customer_id", "video_id", "due_date", "videos_checked_out_count", "available_inventory"].sort
 
       # Assert
       expect {
-        post videos_path, params: video_hash
-      }.must_change "Video.count", 1
+        post check_out_path, params: rental_hash
+      }.must_differ "Rental.count", 1
 
-      must_respond_with :created
-    end
+      must_respond_with :ok
 
-    it "will respond with bad request and errors for an invalid movie" do
-      # Arrange
-      video_hash = {
-        title: "Alf the movie",
-        overview: "The most early 90s movie of all time",
-        release_date: "December 16th 2025",
-        total_inventory: 6,
-        available_inventory: 6
-      }
-  
-      video_hash[:title] = nil
-  
-      # Assert
-      expect {
-        post videos_path, params: video_hash
-      }.wont_change "Video.count"
       body = JSON.parse(response.body)
 
-      expect(body.keys).must_include "errors"
-      expect(body["errors"].keys).must_include "title"
-      expect(body["errors"]["title"]).must_include "can't be blank"
+      expect(body.keys.sort).must_equal fields
+
+      expect(Date.parse(body["due_date"]) - Date.today).must_equal 7
+      expect(body["videos_checked_out_count"]).must_equal before_videos_count + 1
+      expect(body["available_inventory"]).must_equal before_inventory - 1
+    end
+
+    it "will respond with not found and errors for an invalid customer" do
+      # Arrange
+      rental_hash[:customer_id] = nil
   
-      must_respond_with :bad_request
+      # Assert
+      expect {
+        post check_out_path, params: rental_hash
+      }.wont_change "Rental.count"
+
+      body = JSON.parse(response.body)
+      expect(body.keys).must_include "errors"
+      expect(body["errors"]).must_include "Not Found"
+  
+      must_respond_with :not_found
+    end
+
+    it "will respond with not found and errors for an invalid video" do
+      # Arrange
+      rental_hash[:video_id] = nil
+  
+      # Assert
+      expect {
+        post check_out_path, params: rental_hash
+      }.wont_change "Rental.count"
+
+      body = JSON.parse(response.body)
+      expect(body.keys).must_include "errors"
+      expect(body["errors"]).must_include "Not Found"
+  
+      must_respond_with :not_found
     end
   end
 end
