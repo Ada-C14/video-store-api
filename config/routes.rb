7cfc4913b413c1952c0af738125d0dac7e9e1de9
dnsrocha <drocha@gmail.com>
2020-12-03 08:28:18 -0800

    added routes for check in/check out

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :customers, only: [:index, :show, :create]
   resources :videos, only: [:index, :show, :create]
 
-  ## get '/zomg', to: 'application#show'
+  post '/rentals/check-out', to: 'rentals#check_out', as: 'check-out'
+  post '/rentals/check-in', to: 'rentals#check_in', as: 'check-in'
 end
