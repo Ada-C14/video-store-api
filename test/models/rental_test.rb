2a4f1acefb6e0d13801e63cd8e18fa39f5696eab
Alice D <alicedilanchian@gmail.com>
2020-12-03 14:48:03 -0500

    rental model tests - relations - must respond to videos

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 describe Rental do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "relations" do
+
+    it "must respond to videos" do
+      rental_1 = rentals(:rental_1)
+      expect(rental_1).must_respond_to :videos
+      rental_1.rentals.each do |video|
+        expect(video).must_be_kind_of Video
+      end
+    end
+
+
+  end
+
 end
