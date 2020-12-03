0a8bae56e3851ea2bf54992d48be1c4d553f9ff4
Anna Kim <anna.kim93@gmail.com>
2020-12-02 19:34:49 -0500

    add index action

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,2 +1,6 @@
 class VideosController < ApplicationController
+  def index
+    videos = Videos.all.as_json(only: [:id, :title, :release_date, :available_inventory])
+    render json: videos, status: :ok
+  end
 end
