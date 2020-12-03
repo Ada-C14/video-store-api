c47c7cf42b469fac954710e33a04241cdfcdbcc7
gessica <gessicamohr18@gmail.com>
2020-12-02 18:07:38 -0700

    added validations to rental model, deleted a column for rental table - videocheckedoutcount

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,14 @@
 class RentalsController < ApplicationController
+
+  # def check_in
+  #   customer = Customer.
+  # end
+
+
+
+  private
+
+  def rental_params
+    return params.permit(:video_id, :customer_id)
+  end
 end
