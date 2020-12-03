f10ffe57d63afbe141366c8d800a29b1a8800566
KayKay-git <kayalew@yahoo.com>
2020-12-02 22:25:06 -0500

    Wrote rental test blocks

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -7,6 +7,28 @@ describe RentalsController do
   # end
 
   describe 'check_in' do
+    it 'can check-in a video that was checked out' do
 
+    end
+
+    it 'increases a video available inventory count by 1 when checked in' do
+
+    end
+
+    it 'decreases a customer videos_checked_out_count by 1 when checked in' do
+
+    end
+
+    it 'sends a bad_request for an invalid customer' do
+
+    end
+
+    it 'sends a bad_request for an invalid video' do
+
+    end
+
+    it 'sends a not_found status for a rental that does not exist' do
+
+    end
   end
 end
