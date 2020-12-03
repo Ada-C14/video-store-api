f43f96072f17fbd7233c14b9cf950d101a0f0f7c
Alice D <alicedilanchian@gmail.com>
2020-12-03 10:13:16 -0500

    customer model - relations - has rentals test

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 describe Customer do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+
+  describe "relations" do
+
+    it "has rentals" do
+      customer_1 = customers(:customer_1)
+      expect(customer_1).must_respond_to :rentals
+      customer_1.rentals.each do |rental|
+        expect(rental).must_be_kind_of Rental
+      end
+    end
+
+
+
+  end
+
 end
