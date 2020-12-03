f1c2ea45db33febe94498451330663b785d13021
marks214 <aimee.oz.x@gmail.com>
2020-12-03 12:44:04 -0800

    Wave2 postman tests passing

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,79 @@
 class RentalsController < ApplicationController
+
+  def check_out
+    customer = Customer.find_by(id: params[:customer_id])
+    video = Video.find_by(id: params[:video_id])
+
+    if customer.nil? || video.nil?
+      render json: {
+        errors: [
+          'Not Found'
+        ]
+      }, status: :not_found
+      return
+    end
+
+    rental = Rental.new(customer_id: customer.id, video_id: video.id)
+    result = rental.save
+
+    if result
+      rental.due_date = Date.current + 7
+      rental.save
+      customer.videos_checked_out_count += 1
+      customer.save
+      video.available_inventory -= 1
+      video.save
+      render json: {
+        customer_id: rental.customer_id,
+        video_id: rental.video_id,
+        due_date: rental.due_date,
+        videos_checked_out_count: customer.videos_checked_out_count,
+        available_inventory: video.available_inventory
+      }, status: :ok
+    else
+      render json: {
+        errors: rental.errors.messages
+      }, status: :bad_request
+      return
+    end
+
+  end
+
+  def check_in
+    customer = Customer.find_by(id: params[:customer_id])
+    video = Video.find_by(id: params[:video_id])
+
+    if customer.nil? || video.nil?
+      render json: {
+        errors: [
+          'Not Found'
+        ]
+      }, status: :not_found
+      return
+    else
+      customer.videos_checked_out_count -= 1
+      customer.save
+      video.available_inventory += 1
+      video.save
+      render json: {
+        customer_id: customer.id,
+        video_id: video.id,
+        videos_checked_out_count: customer.videos_checked_out_count,
+        available_inventory: video.available_inventory
+      }, status: :ok
+      return
+    end
+
+      render json: {
+        errors: [
+          'Bad Request'
+        ]
+      }, status: :bad_request
+      return
+  end
+
+  private
+  def rental_params
+    params.permit(:customer_id, :movie_id, :due_date)
+  end
 end
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,41 @@
 class Rental < ApplicationRecord
   belongs_to :video
   belongs_to :customer
+
+  validates :video_id, presence: true
+  validates :customer_id, presence: true
+
+
+  #TODO: need to make these work
+  def self.checkout(customer, video)
+    self.due_date = Date.current + 7
+    self.save
+    customer.videos_checked_out_count += 1
+    customer.save
+    video.available_inventory -= 1
+    video.save
+    return #self #customer.videos_checked_out_count, video.available_inventory
+  end
+
+  def checkin
+    rental = Rental.find_by(params[:customer_id], video_id: params[:video_id])
+    customer = Customer.find_by(id: params[:customer_id])
+    video = Video.find_by(id: params[:video_id])
+    if customer && video
+      customer.videos_checked_out_count -= 1
+      customer.save
+      video.available_inventory += 1
+      video.save
+    else
+      return nil
+    end
+    return {
+      customer_id: customer.id,
+      video_id: video.id,
+      due_date: rental.due_date,
+      videos_checked_out_count: customer.videos_checked_out_count,
+      available_inventory: video.available_inventory
+    }
+  end
+
 end
