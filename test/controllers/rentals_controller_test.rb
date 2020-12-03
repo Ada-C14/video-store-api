91f8b90647fa44883589e52f6716a6aa069f8149
Tram Bui <pbui17@cmc.edu>
2020-12-03 11:18:28 -0600

    added controller tests to rental controller

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,7 +1,47 @@
 require "test_helper"
 
 describe RentalsController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  before do
+    @video = videos(:inception)
+    @customer = customers(:customer_one)
+  end
+  describe "check out" do
+    it "can create a valid rental when checking out" do
+      rental_hash = {
+          customer: @customer,
+          video: @video,
+          due_date: Time.now + 7.days
+      }
+
+      expect {
+        post check_out_path, params: rental_hash
+      }.must_differ "Rental.count", 1
+
+      must_respond_with :created
+    end
+
+    it "will respond with bad request and errors for invalid params" do
+      rental_hash = {
+          customer: @customer,
+          video: @video,
+          due_date: Time.now + 7.days
+      }
+
+      rental_hash[:customer] = nil
+
+      expect {
+        post check_out_path, params: rental_hash
+      }.wont_differ "Rental.count"
+      body = JSON.parse(response.body)
+
+      expect(body.keys).must_include "errors"
+      expect(body["errors"].keys).must_include "customer"
+      must_respond_with :bad_request
+
+    end
+  end
+
+  describe "check in" do
+
+  end
 end
