30a7ae03d3b9329981ee9658d5030921f2edfca2
Emily Nagai <emnagai89@gmail.com>
2020-12-03 11:38:52 -0800

    modified custom rentals route and rental custom method

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,14 @@
 class Rental < ApplicationRecord
   belongs_to :customer
   belongs_to :video
+
+  # validates :customer_id, presence: true
+  # validates :video_id, presence: true
+
+  def checked_in_time
+    if self.customer_id.check_in && self.video_id.check_in
+      return self.checked_in
+     end
+    self.save
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   #
-  post 'rentals/checked_in'
+  post 'rentals/check_in_rental'
   resources :rentals, only: [:create]
   resources :customers, only: [:index, :show, :create]
   resources :videos, only: [:index, :show, :create]
