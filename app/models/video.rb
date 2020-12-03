0a9b2a9f59876e8737b131888f7ea21350ed093c
Noor B.A <noor.bahr.aluloom@gmail.com>
2020-12-03 14:50:14 -0800

    added video model validations test

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,5 +2,6 @@ class Video < ApplicationRecord
   has_many :rentals
   has_many :customers
 
-  validates :title, :overview, :release_date, :total_inventory, :available_inventory, presence: true
+  validates :title, :overview, :release_date, presence: true
+  validates :total_inventory, :available_inventory, presence: true, numericality: { greater_than_or_equal_to: 0}
 end
