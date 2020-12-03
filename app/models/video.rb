57492641777da9dc3bb7e308322a4cd14ed04d3f
Jasmine <jasylop@gmail.com>
2020-12-02 17:50:20 -0800

    added numericality validation

--- app/models/video.rb
+++ app/models/video.rb
@@ -2,7 +2,7 @@ class Video < ApplicationRecord
   validates :title, presence: true
   validates :overview, presence: true
   validates :release_date, presence: true
-  validates :total_inventory, presence: true
-  validates :available_inventory, presence: true
+  validates :total_inventory, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :available_inventory, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0, message: "This video is out of stock"}
   has_many :rentals
 end
