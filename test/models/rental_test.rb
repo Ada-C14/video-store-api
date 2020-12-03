cf9ff0a3f5334edb084fd34545a1c7f911c0f4b8
Taylor Tompkins <taylormililani@gmail.com>
2020-12-02 23:13:56 -0800

    relationship model tests for vid and rental

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 describe Rental do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "relationships" do
+    it "can have one customer and one video" do
+      customer = customers(:customer_two)
+      video = videos(:black_widow)
+      rental = Rental.new(due_date: Date.today + 7,
+                          customer_id: customer.id,
+                          video_id: video.id,
+                          videos_checked_out_count: customer.videos_checked_out_count,
+                          available_inventory: video.available_inventory)
+
+      expect(rental.video).must_be_instance_of Video
+      expect(rental.customer).must_be_instance_of Customer
+    end
+  end
 end
