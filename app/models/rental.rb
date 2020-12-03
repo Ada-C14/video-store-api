3f6a0b5da9bd59da1ccf0c1969a8c0121c101043
Alice D <alicedilanchian@gmail.com>
2020-12-03 18:19:43 -0500

    checkout rental business logic method

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -8,8 +8,26 @@ class Rental < ApplicationRecord
   validates :due_date, presence: true
 
 
-  def self.checkout()
+  def self.checkout(video, customer)
+    # is available_inventory > 0?
+    return nil unless video.available_inventory >= 1
 
+    time = DateTime.now
+    # time = Date.today
+    due_date = time + 7.days
+
+    # create a rental
+    new_rental = Rental.create(customer_id: customer.id, video_id: video.id, checkout_date: time, due_date: due_date)
+
+    # decrement the available_inventory of video
+    video.available_inventory -= 1
+    video.save
+
+    # increment the video_checkout_count of customer
+    customer.videos_checked_out_count += 1
+    customer.save
+
+    return new_rental
   end
 
 end
