57dfcdc73b1ecc3f32d6f824f9d832ff99566c3f
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-12-02 16:14:56 -0800

    added methods for customers index and videos show

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,2 +1,12 @@
 class VideosController < ApplicationController
+  def show
+    video = Video.find_by(id: params[:id])
+    if video.nil?
+      render json: { errors: "Not Found" },
+              status: :not_found
+      return
+    end
+    render json: video.as_json(only: [:title, :overview, :release_date, :total_inventory, :available_inventory]),
+            status: :ok
+  end
 end
