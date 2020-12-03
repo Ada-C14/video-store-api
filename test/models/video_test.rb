f9177e89c357704d30d90edf57fcf737562804fd
Alice D <alicedilanchian@gmail.com>
2020-12-03 10:39:01 -0500

    video  model - relations - has customers

--- test/models/video_test.rb
+++ test/models/video_test.rb
@@ -4,12 +4,21 @@ describe Video do
   describe "relations" do
 
     it "has rentals" do
-      video_one = customers(:wonder_woman)
+      video_one = videos(:wonder_woman)
       expect(video_one).must_respond_to :rentals
       video_one.rentals.each do |rental|
         expect(rental).must_be_kind_of Rental
       end
     end
+
+    it "has customers through rentals" do
+      video_one = videos(:wonder_woman)
+      expect(video_one).must_respond_to :customers
+      video_one.videos.each do |customer|
+        expect(customer).must_be_kind_of Customer
+      end
+    end
+
 
   end
 end
