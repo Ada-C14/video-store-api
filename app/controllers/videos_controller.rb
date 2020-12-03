1ee1b347bbe3ef673d5f535ab7495e0cf93445ea
marks214 <aimee.oz.x@gmail.com>
2020-12-02 17:55:36 -0800

    show action written to pass postman test

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -9,9 +9,29 @@ class VideosController < ApplicationController
         available_inventory
       ]
     ), status: :ok
+    return
   end
 
   def show
+    video = Video.find_by(id: params[:id])
+
+    if video.nil?
+      render json: {
+        errors: ['Not Found'],
+      }, status: :not_found
+      return
+    end
+
+    render json: video.as_json(
+      only: %i[
+      title
+      overview
+      release_date
+      total_inventory
+      available_inventory
+      ]
+    ), status: :ok
+    return
   end
 
   def create
