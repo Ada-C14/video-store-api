28e20769f4da6fa21feebdb28d44ab82f20a7324
Sophia Donan <sophiadonan@gmail.com>
2020-12-03 07:27:33 -0800

    Wave1: added video params and video create to pass tests

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -15,5 +15,24 @@ class VideosController < ApplicationController
   end
 
   def create
+    video = Video.new(video_params)
+
+    if video.save
+      render json: video.as_json(only: [:id]), status: :created
+      return
+    else
+      render json: {
+          ok: false,
+          errors: video.errors.messages
+      }, status: :bad_request
+      return
+    end
+
+  end
+
+  private
+
+  def video_params
+    return params.permit(:id, :title, :overview, :release_date, :total_inventory, :available_inventory)
   end
 end
