e85d0fff06a282b2e71723e69a6ad79258b7d130
Jasmine <jasylop@gmail.com>
2020-12-02 17:49:53 -0800

    added check-out route

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
 
   resources :videos, only: [:index, :show, :create]
   resources :customers, only: [:index]
-
+  post '/rentals/check-out', to: 'rentals#check_out', as: 'check_out'
 end
