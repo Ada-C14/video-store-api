130ef4e0b8eb3c9017466be0f70a4b08198157a6
Tram Bui <pbui17@cmc.edu>
2020-12-02 19:56:13 -0600

    alter the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_02_033556) do
+ActiveRecord::Schema.define(version: 2020_12_03_015441) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,16 @@ ActiveRecord::Schema.define(version: 2020_12_02_033556) do
     t.integer "videos_checked_out_count"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "rentals", force: :cascade do |t|
+    t.date "due_date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "video_id"
+    t.bigint "customer_id"
+    t.index ["customer_id"], name: "index_rentals_on_customer_id"
+    t.index ["video_id"], name: "index_rentals_on_video_id"
   end
 
   create_table "videos", force: :cascade do |t|
