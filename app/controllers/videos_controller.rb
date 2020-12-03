f669191cd18bf31cfefd219b66eade2432754b6e
Lina Do <l.do5147@gmail.com>
2020-12-02 16:51:01 -0800

    added render show action for a found video

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,6 +1,17 @@
 class VideosController < ApplicationController
 
   def show
+    video = Video.find_by(id: params[:id])
 
+    if video.nil?
+      render json: {
+          ok: false,
+          message: 'Not found'
+      }, status: :not_found
+
+      return
+    end
+
+    render json: video.as_json(only [:title, :overview, :release_date, :total_inventory])
   end
 end
