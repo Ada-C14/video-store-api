8b68dcb02b450a8334ae07216473aba62aba2c0e
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-12-03 00:01:44 -0800

    added method for check_out action for rental

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,9 +1,34 @@
 class RentalsController < ApplicationController
   def check_out
-    rental = Rental.new(rental_params)
+    customer = Customer.find_by(id: params[:customer_id])
+    video = Video.find_by(id: params[:video_id])
 
-    if rental.save
-      render json: rental.as_json(only: [:id]), status: :created
+    if customer.nil?
+      render json: { errors: ["Not Found"] },
+             status: :not_found
+      return  
+    elsif video.nil?
+      render json: { errors: ["Not Found"] },
+             status: :not_found
+      return
+    end
+
+    rental = Rental.new(rental_params)
+    customer.checkout_update
+    video.checkout_update
+    rental.checkout_update
+
+    if rental.save 
+      response = {customer_id: rental.customer_id, 
+                  video_id: rental.video_id, 
+                  due_date: rental.due_date, 
+                  videos_checked_out_count: customer.videos_checked_out_count,
+                  available_inventory: video.available_inventory
+                }
+
+      render json: response.as_json, status: :ok
+
+    # render json: rental.as_json(include: {customer: {only: [:videos_checked_out_count]}, video: {only: [:available_inventory]}}, only: [:customer_id, :video_id, :due_date]), status: :ok
     else
       render json: { errors: rental.errors.messages }, status: :bad_request
     end
