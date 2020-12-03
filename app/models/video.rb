342a7631abadbfc292ebe55faaa66e56dd04f862
roshni-patel <rpatel01@wesleyan.edu>
2020-12-03 17:49:30 -0600

    Updated has many relationships in customers and videos

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,6 +1,5 @@
 class Video < ApplicationRecord
   has_many :rentals
-  has_many :customers
 
   validates :title, :overview, :release_date, :total_inventory, :available_inventory, presence: true
 end
