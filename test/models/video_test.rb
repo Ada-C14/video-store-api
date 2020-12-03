5627c9c74bd1b9dd6990fc0ba3b5cc5e80d668aa
Iris Lux <iris.lux0@gmail.com>
2020-12-02 20:29:29 -0800

    validation test for video title

--- test/models/video_test.rb
+++ test/models/video_test.rb
@@ -1,7 +1,23 @@
 require "test_helper"
 
 describe Video do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+
+  before do
+    @video = videos(:wonder_woman)
+  end
+  describe 'validations' do
+    it 'id valid when all fields are present' do
+      expect(@video.valid?).must_equal true
+    end
+
+    it 'will be invalid without title' do
+
+      @video.title = nil
+
+      expect(@video.valid?).must_equal false
+      expect(@video.errors.messages).must_include :title
+      expect(@video.errors.messages[:title][0]).must_equal "can't be blank"
+
+    end
+  end
 end
