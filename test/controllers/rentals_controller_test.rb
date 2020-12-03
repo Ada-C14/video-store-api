a9f20b1bba0feef0acdee93973f6a8eee4b2bc87
Taylor Tompkins <taylormililani@gmail.com>
2020-12-02 22:47:40 -0800

    check in smoke and controller tests passing

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -62,15 +62,64 @@ describe RentalsController do
     end
   end
 
-  # describe "check-in" do
-  #   it "successfully checks in a video and correctly modifies customer and video data" do
-  #     rental = Rental.create(customer_id: @customer.id, video_id: @video.id, due_date: "2020-12-9", videos_checked_out_count: @customer.videos_checked_out_count + 1, available_inventory: @video.available_inventory - 1)
-  #
-  #     expect{
-  #       post checkout_path(rental.id)
-  #     }.wont_change 'Rental.count'
-  #
-  #     expect{@customer.videos_checked_out_count}.must_change 1
-  #   end
-  # end
+  describe "check-in" do
+    it "successfully checks in a video and correctly modifies customer and video data" do
+      initial_customer_rentals = @customer.videos_checked_out_count
+      initial_video_inventory = @video.available_inventory
+
+      rental_hash = {
+          customer_id: @customer.id,
+          video_id: @video.id,
+      }
+      expect{
+        post check_in_path, params: rental_hash
+      }.wont_change 'Rental.count'
+
+      expect((initial_customer_rentals - 1) == @customer.reload.videos_checked_out_count).must_equal true
+      expect((initial_video_inventory + 1) == @video.reload.available_inventory).must_equal true
+
+      body = JSON.parse(response.body)
+      expect(body.keys).must_include "customer_id", "video_id"
+      expect(body.keys).must_include "videos_checked_out_count", "available_inventory"
+      must_respond_with :ok
+    end
+
+    it 'it will respond with not found and errors for invalid customer' do
+      # Arrange
+      rental_hash = {
+          customer_id: -1,
+          video_id: @video.id,
+      }
+
+      # Assert
+      expect {
+        post checkout_path, params: rental_hash
+      }.wont_change @video.available_inventory
+
+      body = JSON.parse(response.body)
+
+      expect(body.keys).must_include "errors"
+
+      must_respond_with :not_found
+    end
+
+    it 'it will respond with not found and errors for invalid video' do
+      # Arrange
+      rental_hash = {
+          customer_id: @customer.id,
+          video_id: -1,
+      }
+
+      # Assert
+      expect {
+        post checkout_path, params: rental_hash
+      }.wont_change @customer.videos_checked_out_count
+
+      body = JSON.parse(response.body)
+
+      expect(body.keys).must_include "errors"
+
+      must_respond_with :not_found
+    end
+  end
 end
