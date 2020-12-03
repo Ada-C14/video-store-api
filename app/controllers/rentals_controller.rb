0063d64ed1867f0039d012d350b3a921dba4ab69
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-12-02 17:09:16 -0800

    drafted check-out for rentals

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,17 @@
 class RentalsController < ApplicationController
+  def check_out
+    rental = Rental.new(rental_params)
+
+    if rental.save
+      render json: rental.as_json(only: [:id]), status: :created
+    else
+      render json: { errors: rental.errors.messages }, status: :bad_request
+    end
+  end
+
+  private
+
+  def rental_params
+    params.permit(:title, :overview, :release_date, :total_inventory, :available_inventory)
+  end
 end
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,7 +1,48 @@
 require "test_helper"
 
 describe RentalsController do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "check-out" do
+    it "can check-out a valid video" do
+      # Arrange
+      video_hash = {
+        title: "Alf the movie",
+        overview: "The most early 90s movie of all time",
+        release_date: "December 16th 2025",
+        total_inventory: 6,
+        available_inventory: 6
+      }
+
+      # Assert
+      expect {
+        post videos_path, params: video_hash
+      }.must_change "Video.count", 1
+
+      must_respond_with :created
+    end
+
+    it "will respond with bad request and errors for an invalid movie" do
+      # Arrange
+      video_hash = {
+        title: "Alf the movie",
+        overview: "The most early 90s movie of all time",
+        release_date: "December 16th 2025",
+        total_inventory: 6,
+        available_inventory: 6
+      }
+  
+      video_hash[:title] = nil
+  
+      # Assert
+      expect {
+        post videos_path, params: video_hash
+      }.wont_change "Video.count"
+      body = JSON.parse(response.body)
+
+      expect(body.keys).must_include "errors"
+      expect(body["errors"].keys).must_include "title"
+      expect(body["errors"]["title"]).must_include "can't be blank"
+  
+      must_respond_with :bad_request
+    end
+  end
 end
