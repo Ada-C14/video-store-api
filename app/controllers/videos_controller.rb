bd4783ea74bb8514f9c50a4525f0007ebff47e3d
marks214 <aimee.oz.x@gmail.com>
2020-12-02 18:27:23 -0800

    updated videos create action

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -35,5 +35,19 @@ class VideosController < ApplicationController
   end
 
   def create
+    video = Video.new(video_params)
+    if video.save
+      render json: video.as_json(only: [:id]), status: :created #201
+      return
+    else
+      render json: {
+        errors: video.errors.messages
+      }, status: :bad_request
+      return
+    end
+  end
+
+  def video_params
+    return params.require(:video).permit(:title, :overview, :release_date, :total_inventory, :available_inventory)
   end
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -2,7 +2,7 @@ class Video < ApplicationRecord
   has_many :rentals
   has_many :customers, through: :rentals
 
-  validates :title, presence: true
-  validates :release_date, presence: true
-  validates :available_inventory, presence: true # integer?
+  # validates :title, presence: true
+  # validates :release_date, presence: true
+  # validates :available_inventory, presence: true # integer?
 end
