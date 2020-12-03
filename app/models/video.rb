b2e29458a67ce47828858bb86c9fd2394bdeaaab
Anna Kim <anna.kim93@gmail.com>
2020-12-02 21:55:43 -0500

    revising validations for video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,7 +1,14 @@
 class Video < ApplicationRecord
-  validates :title, presence: true
-  validates :overview, presence: true
-  validates :release_date, presence: true
-  validates :total_inventory, presence: true, numericality: true
-  validates :available_inventory, presence: true, numericality: true
+  validates :title,
+            presence: true
+  validates :overview,
+            presence: true
+  validates :release_date,
+            presence: true
+  validates :total_inventory,
+            presence: true,
+            numericality: { greater_than_or_equal_to: 0 }
+  validates :available_inventory,
+            presence: true,
+            numericality: { greater_than_or_equal_to: 0 }
 end
