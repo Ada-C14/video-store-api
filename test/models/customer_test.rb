30222421e6a9aeb1961485d677a310919782c5a1
Alice D <alicedilanchian@gmail.com>
2020-12-03 10:17:01 -0500

    customer model - relations - has videos through rentals

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -5,10 +5,18 @@ describe Customer do
   describe "relations" do
 
     it "has rentals" do
-      customer_one = customers(:customer_1)
+      customer_one = customers(:customer_one)
       expect(customer_one).must_respond_to :rentals
       customer_one.rentals.each do |rental|
         expect(rental).must_be_kind_of Rental
+      end
+    end
+
+    it "has videos through rentals" do
+      customer_one = customers(:customer_one)
+      expect(customer_one).must_respond_to :videos
+      customer_one.videos.each do |video|
+        expect(video).must_be_kind_of Video
       end
     end
 
