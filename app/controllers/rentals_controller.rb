67c62df25efcb1817132e4b79e3470975d731c27
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-12-02 20:58:41 -0800

    fix spacing and indentation in rentals_controller.rb

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -2,21 +2,26 @@ class RentalsController < ApplicationController
   def check_in
     customer = Customer.find_by(id: params[:customer_id])
     video = Video.find_by(id: params[:video_id])
+
     if video.nil? || customer.nil?
       render json: {
           errors: ['Not Found'],
       }, status: :not_found
       return
     end
-    rental = Rental.find_by(customer_id: customer.id, video_id: video.id)
+
+    rental = Rental.find_by(rental_params)
+
     if rental.nil?
       render json: {
           errors: 'Not found',
       }, status: :not_found
       return
     end
+
     video.increase_available_inventory
     customer.decrease_video_checked_out_count
+
     render json: rental.as_json(only: [:customer_id, :video_id]).merge({videos_checked_out_count: customer.videos_checked_out_count, available_inventory: video.available_inventory}),
              status: :ok
   end
