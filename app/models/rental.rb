8569a6fe60c49e2f07404c1b3df6cccf1453bc2c
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-12-02 22:54:56 -0800

    revised the validates

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -2,5 +2,5 @@ class Rental < ApplicationRecord
   belongs_to :customer
   belongs_to :video
 
-  validates :due_date, :available_inventory, :customer_id, :video_id, presence: true
+  validates :customer_id, :video_id, presence: true
 end
