95218d24f25271ca8adabd46aae7dc2b5cbca5a2
Rachael Gomez <gomezrc1220@gmail.com>
2020-12-02 17:18:33 -0800

    added validations back

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,7 +2,7 @@ class Customer < ApplicationRecord
   has_many :rentals
   has_many :videos, through: :rentals
 
-  # validates :name, presence: true
+  validates :name, presence: true
 
   def increase_video_checked_out_count
     self.videos_checked_out_count += 1
--- app/models/video.rb
+++ app/models/video.rb
@@ -2,11 +2,11 @@ class Video < ApplicationRecord
   has_many :rentals
   has_many :customers, through: :rentals
 
-  # validates :title, presence: true
-  # validates :overview, presence: true
-  # validates :release_date, presence: true
-  # validates :total_inventory, presence: true
-  # validates :available_inventory, presence: true
+  validates :title, presence: true
+  validates :overview, presence: true
+  validates :release_date, presence: true
+  validates :total_inventory, presence: true
+  validates :available_inventory, presence: true
 
   def decrease_available_inventory
     self.available_inventory -= 1
