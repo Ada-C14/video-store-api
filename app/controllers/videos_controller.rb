711825427370dd2fad2efc3779795dcb00ba71aa
Anna Kim <anna.kim93@gmail.com>
2020-12-02 20:12:44 -0500

    add route for videos; pass videos_controller index action test

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,4 +3,9 @@ class VideosController < ApplicationController
     videos = Video.all.as_json(only: [:id, :title, :release_date, :available_inventory])
     render json: videos, status: :ok
   end
+
+  def show
+    video = Video.find_by(id: params[:id]).as_json(only: [:id, :title, :release_date, :available_inventory])
+    render json: video, status: :ok
+  end
 end
