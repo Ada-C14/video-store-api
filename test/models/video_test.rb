e892088905895684acfd9b7cd59b0780cfe1d9bb
Anna Kim <anna.kim93@gmail.com>
2020-12-02 23:19:49 -0500

    video model validation tests

--- test/models/video_test.rb
+++ test/models/video_test.rb
@@ -25,8 +25,50 @@ describe Video do
   end
 
   describe 'validations' do
-    it '' do
+    it 'must have a title' do
+      video.title = nil
+      expect(video.valid?).must_equal false
+      expect(video.errors.messages).must_include :title
+    end
 
+    it 'must have a overview' do
+      video.overview = nil
+      expect(video.valid?).must_equal false
+      expect(video.errors.messages).must_include :overview
+    end
+
+    it 'must have a release date' do
+      video.release_date = nil
+      expect(video.valid?).must_equal false
+      expect(video.errors.messages).must_include :release_date
+    end
+
+    it 'must have a total inventory number' do
+      video.total_inventory = nil
+      expect(video.valid?).must_equal false
+      expect(video.errors.messages).must_include :total_inventory
+    end
+
+    it 'must have a total inventory number greater than or equal to 0' do
+      video.total_inventory = -1
+
+      expect(video.valid?).must_equal false
+      expect(video.errors.messages).must_include :total_inventory
+      expect(video.errors.messages[:total_inventory]).must_include 'must be greater than or equal to 0'
+    end
+
+    it 'must have an available inventory number' do
+      video.available_inventory = nil
+      expect(video.valid?).must_equal false
+      expect(video.errors.messages).must_include :available_inventory
+    end
+
+    it 'must have an available inventory number greater than or equal to 0' do
+      video.available_inventory = -1
+
+      expect(video.valid?).must_equal false
+      expect(video.errors.messages).must_include :available_inventory
+      expect(video.errors.messages[:available_inventory]).must_include 'must be greater than or equal to 0'
     end
   end
 end
