ed1d8ab064542aad95865d863f2ad425b5897b24
Madeline <madelign@gmail.com>
2020-12-03 21:59:37 -0500

    removed customer show method. not needed

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -2,24 +2,7 @@ class CustomersController < ApplicationController
   def index
     customers = Customer.all.order(:name)
     render json: customers.as_json(only: [:id, :name, :registered_at, :postal_code,
-                                     :phone, :videos_checked_out_count]), status: :ok
-  end
-
-  def show
-    customer = Customer.find_by(id: params[:id])
-
-    if customer.nil?
-      render json: {
-        ok: false,
-        message: "Not found",
-      }, status: :not_found
-
-      return
-    end
-
-    render json: customer.as_json(only: [:id, :name, :registered_at, :postal_code,
-                                         :phone, :videos_checked_out_count]),
-           status: :ok
+                                          :phone, :videos_checked_out_count]), status: :ok
   end
 
 end
