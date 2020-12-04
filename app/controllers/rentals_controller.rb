1d7f33857c59cafebbf0a17f323ac37ea4e06bd5
Stacy <lundquist.stacy@gmail.com>
2020-12-03 16:50:50 -0800

    added controller methods

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -8,17 +8,16 @@ class RentalsController < ApplicationController
       rental.customer.save!
 
       rental.video.available_inventory -= 1
-      # rental[:checked_out] = true
 
       if rental.video.save
         return render json: rental.as_json(only: [:customer_id, :video_id, :due_date]).merge(
 
-          videos_checked_out_count: rental.customer.videos_checked_out_count,
-          available_inventory: rental.video.available_inventory
+            videos_checked_out_count: rental.customer.videos_checked_out_count,
+            available_inventory: rental.video.available_inventory
         ), status: :ok
       else
         rental.destroy # i want to destroy the rental created because video is invalid
-        return render json: { errors: rental.video.errors.messages }, status: :bad_request
+        return render json: {errors: rental.video.errors.messages}, status: :bad_request
 
       end
 
@@ -30,57 +29,21 @@ class RentalsController < ApplicationController
 
   def check_in
     rental = Rental.find_by(video_id: params[:video_id], customer_id: params[:customer_id])
-    if rental
-      rental.customer.videos_checked_out_count -= 1
-      rental.customer.save!
-
-      rental.video.available_inventory += 1
-      rental.video.save!
-      rental[:checked_out] = false
-
-      if rental.save
-
-        return render json: rental.as_json(only: [:customer_id, :video_id]).merge(
-            videos_checked_out_count: rental.customer.videos_checked_out_count,
-            available_inventory: rental.video.available_inventory
-        )
-      else
-        return render json: {errors: rental.video.errors.messages}, status: :bad_request
-      end
-
-    else
+    if rental.nil?
       return render json: {errors: ["Not Found"]}, status: :not_found
     end
+
+    if rental.checkin
+
+      return render json: rental.as_json(only: [:customer_id, :video_id]).merge(
+          videos_checked_out_count: rental.customer.videos_checked_out_count,
+          available_inventory: rental.video.available_inventory
+      )
+    else
+      return render json: {errors: rental.video.errors.messages}, status: :bad_request
+    end
+
   end
-
-
-    # if (rental = Rental.find_by(video_id: params[:video_id], customer_id: params[:customer_id], checked_out: true))
-    #   rental[:checked_out] = false
-    #   rental[:due_date] = nil
-    #   rental[:checkin_date] = Date.today
-    #   rental.video.available_inventory += 1
-    #   rental.save
-    #   if rental.save
-    #     return render json: rental.as_json(only: [:customer_id, :video_id, :videos_checked_out_count, :checkin_date]).merge(
-    #         available_inventory: rental.video.available_inventory)
-    #   else
-    #     return render json: {errors: rental.video.errors.messages}, status: :bad_request
-    #   end
-    #
-    # else
-    #   return render json: {errors: ["Not Found"]}, status: :not_found
-    #
-    # end
-    # rental = Rental.find_by(rental_params)
-    # if rental
-    #   rental.customer.videos_checked_out_count -= 1
-    #   rental.video.available_inventory += 1
-    # else
-    #   return render json: {errors: rental.video.errors.messages}, status: :bad_request
-    # end
-
-
-
 
 
   private
@@ -89,4 +52,5 @@ class RentalsController < ApplicationController
     params.permit(:customer_id, :video_id)
   end
 
-end 
+
+end
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,6 +1,37 @@
 class Rental < ApplicationRecord
   belongs_to :customer
   belongs_to :video
+
+  def checkout
+
+    customer.videos_checked_out_count += 1
+    customer.save!
+
+    video.available_inventory -= 1
+    video.save!
+
+    #   rental[:checked_out] = true
+    #   rental[:due_date] = Date.today + 7
+    #   rental[:checkin_date] = nil
+
+    save
+  end
+
+  def checkin
+
+    customer.videos_checked_out_count -= 1
+    customer.save!
+
+    video.available_inventory += 1
+    video.save!
+
+    #   rental[:checked_out] = false
+    #   rental[:due_date] = nil
+    #   rental[:checkin_date] = Date.today
+
+    save
+  end
+
+
 end
 
-# with counter_cache customer.rentals.size will perform the count counter_cache: :videos_checked_out_count
