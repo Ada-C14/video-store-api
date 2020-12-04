f3dad655304bd4679b06bf3090b8a0ec9727c397
India Kato <india.kato@gmail.com>
2020-12-04 12:08:23 -0800

    finished rentals controller tests

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -74,4 +74,58 @@ describe RentalsController do
       expect(body["errors"]).must_include "Bad Request"
     end
   end
+
+  describe "check-in" do
+    it "can successfully check-in" do
+      customer = customers(:customer_one)
+      video = videos(:wonder_woman)
+      rentals_hash = {
+          customer_id: customer.id,
+          video_id: video.id
+      }
+
+      expect {
+        post check_in_path, params: rentals_hash
+      }.wont_change "Rental.count"
+
+      must_respond_with :ok
+    end
+
+    it "will respond with not_found if customer doesn't exist" do
+      video = videos(:wonder_woman)
+      rentals_hash = {
+          customer_id: nil,
+          video_id: video.id
+      }
+
+      expect {
+        post check_in_path, params: rentals_hash
+      }.wont_change "Rental.count"
+
+      must_respond_with :not_found
+      body = JSON.parse(response.body)
+
+      expect(body.keys).must_include "errors"
+      expect(body["errors"]).must_include "Not Found"
+    end
+
+    it "will respond with not_found if video doesn't exist" do
+      customer = customers(:customer_one)
+      rentals_hash = {
+          customer_id: customer.id,
+          video_id: nil
+      }
+
+      expect {
+        post check_in_path, params: rentals_hash
+      }.wont_change "Rental.count"
+
+      must_respond_with :not_found
+
+      body = JSON.parse(response.body)
+
+      expect(body.keys).must_include "errors"
+      expect(body["errors"]).must_include "Not Found"
+    end
+  end
 end
