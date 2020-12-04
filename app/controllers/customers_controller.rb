42a5d77003a52c73603cda7907fe15c54b3a9bb7
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-12-03 21:51:14 -0800

    added paginate params to customers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,10 +1,9 @@
 class CustomersController < ApplicationController
   def index
     customers = Customer.all
+    customers = customers.reorder(params[:sort] => :asc) if params[:sort]
+    customers = customers.paginate(page: params[:p], per_page: params[:n]) if params[:p] || params[:n]
 
-    attribute = params["sort"]
-    customers = Customer.reorder(attribute => :asc) if attribute
-    
     render json: customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :videos_checked_out_count]),
             status: :ok
   end
