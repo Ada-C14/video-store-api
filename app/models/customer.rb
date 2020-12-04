fa319d39fdf8e24529283578c210791508d74eb3
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-12-03 21:02:06 -0800

    added sort params to customers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,8 @@
 class Customer < ApplicationRecord
   has_many :videos
 
+  default_scope { order(id: :asc) }
+
   def checkout_update
     self.update(videos_checked_out_count: self.videos_checked_out_count + 1)
   end
