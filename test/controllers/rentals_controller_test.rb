1008f906aa6656c90c90777f8f1d1f0e44a00011
Alice D <alicedilanchian@gmail.com>
2020-12-04 16:56:26 -0500

    rentals controller test - can checkout a rental; returns status ok, and expected body - skeleton

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -20,6 +20,19 @@ describe RentalsController do
       expect {post check_out_path, params: @rental_hash}.must_change "@rental_hash.customer.videos_checked_out_count", 1
     end
 
+    it "can check_out a rental - returns status ok, and expected body" do
+      post check_out_path, params: @rental_hash
+      body = JSON.parse(response.body)
+      fields = ["customer_id", "video_id", "due_date", "videos_checked_out_count", "available_inventory"].sort
+      expect(body.keys.sort).must_equal fields
+      expect(body["customer_id"]).must_equal ""
+      expect(body["video_id"]).must_equal ""
+      expect(body["due_date"]).must_equal ""
+      expect(body["available_inventory"]).must_equal ""
+      must_respond_with :ok
+    end
+
+
 
 
 
