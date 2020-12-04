f0245c66ff591f5d1c2d4d5671517f9c0072ebac
Ida <ida.goitom@yahoo.com>
2020-12-04 10:04:02 -0800

    updated spacing

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -70,20 +70,6 @@ class RentalsController < ApplicationController
 
   end
 
-  # def create
-  #   rental = Rental.new(rental_params)
-  #   if rental.save
-  #     render json: rental.as_json(only: [:id]), status: :created
-  #   else
-  #     render json: {
-  #         errors: rental.errors.messages
-  #     }, status: :bad_request
-  #     return
-  #   end
-  # end
-
-
-
   def rental_params
     return params.permit(:id, :customer_id, :video_id, :due_date, :videos_checked_out_count,
                          :available_inventory)
