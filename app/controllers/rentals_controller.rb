b0481e6d3f4f02295aaf411346e88a337b9d41dd
Anna Kim <anna.kim93@gmail.com>
2020-12-04 18:12:56 -0500

    adjust rental in rental check in custom action to be a Rental object not Active Record Relation so that customer methods apply

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -44,29 +44,31 @@ class RentalsController < ApplicationController
 
   def check_in_rental
     rental = Rental.where(
-      video_id: params[:video_id],
-      customer_id: params[:customer_id],
+      customer_id: params[:customer_id].to_i,
       checked_in: nil
-    )
+    ).find_by(video_id: params[:video_id].to_i)
 
     if rental.nil?
       render json: { errors: ['Not Found'] }, status: :not_found
-    elsif rental.customer_valid?.nil?
-      render json: { errors: ['Customer Not Found'] }, status: :not_found
-    elsif rental.video_valid?.nil?
-      render json: { errors: ['Video Not Found'] }, status: :not_found
-    else
-      rental.rental_checkin
-
-      render json: rental.as_json(
-        only: [:customer_id, :video_id, :checked_in],
-        include: {
-          customer: { only: [:videos_checked_out_count] },
-          video: { only: [:available_inventory] }
-        }
-      ), status: :ok
-
     end
+
+    unless rental.valid_video? && rental.valid_customer?
+      render json: {
+        errors: ['Not Found']
+      }, status: :not_found
+      return
+    end
+
+    rental.rental_checkin
+
+    render json: {
+      customer_id: rental.customer_id,
+      video_id: rental.video_id,
+      due_date: rental.due_date,
+      available_inventory: rental.video.available_inventory,
+      videos_checked_out_count: rental.customer.videos_checked_out_count
+    }, status: :ok
+
     return
   end
 
