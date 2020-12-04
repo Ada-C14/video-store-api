615b63260e41beb2c17db3eb1aca4d2e1ccc4e5e
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-12-04 10:27:04 -0800

    added only to the before action

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,10 +1,10 @@
 class CustomersController < ApplicationController
-  before_action :sort_column
-  before_action :valid_pagination?
+  before_action :sort_column, only: [:index]
+  before_action :valid_pagination?, only: [:index]
 
   def index
     customers = Customer.all
-    customers = customers.reorder(params[:sort] => :asc) if params[:sort]
+    customers = customers.order(params[:sort] => :asc) if params[:sort]
     customers = customers.paginate(page: params[:p], per_page: params[:n]) if params[:p] || params[:n]
 
     render json: customers.as_json(only: [:id, :name, :registered_at, :postal_code, :phone, :videos_checked_out_count]),
