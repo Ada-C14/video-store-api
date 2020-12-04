ab381fb16891e089a2a91873eb544cd7cac7d8aa
Schanen Ryan <schanen.r@gmail.com>
2020-12-03 21:42:18 -0800

    Still debugging check-in tests.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,7 @@
 class CustomersController < ApplicationController
 
   def index
-    customers = Customer.all.as_json(only: [:id, :name, :phone, :postal_code, :registered_at, :videos_checked_out_count]).order(:id)
+    customers = Customer.all.order(:id).as_json(only: [:id, :name, :phone, :postal_code, :registered_at, :videos_checked_out_count])
     render json: customers, status: :ok
   end
 
