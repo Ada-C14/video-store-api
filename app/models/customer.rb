9e9aa4149ae8af17d04d20d3369135d20a35064c
India Kato <india.kato@gmail.com>
2020-12-04 09:44:26 -0800

    checkin method

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,8 @@
 class Customer < ApplicationRecord
   has_many :rentals
+
+  def self.increase_videos_checked_out
+    self.videos_checked_out_count += 1
+    self.save
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :videos, only: [:index, :show, :create]
   resources :customers, only: [:index]
+
+  post "/rentals/check_in", to: "rentals#check_in", as: "check-in"
 end
