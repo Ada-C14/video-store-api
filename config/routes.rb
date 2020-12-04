24dedda0fa0b75c78b9567d286ef999137b2422f
Alice D <alicedilanchian@gmail.com>
2020-12-03 19:08:11 -0500

    routes updated and rental yml file updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,7 @@
 Rails.application.routes.draw do
-  get 'rentals/index'
-  get 'rentals/show'
-  get 'rentals/create'
-  get 'customers/index'
-  get 'customers/show'
-  get 'customers/create'
-  # get 'customer/index'
-  # get 'customer/show'
-  # get 'customer/create'
-  # get 'videos/index'
-  # get 'videos/show'
-  # get 'videos/create'
+
   resources :videos, only: [:index, :show, :create]
   resources :customers, only: [:index, :show, :create]
-
+  resources :rentals, only: [:index, :show, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
