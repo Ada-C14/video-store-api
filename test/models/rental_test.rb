049b37759ea0ac2d49954ef1e0703c2669bb62ad
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2020-12-03 20:52:30 -0800

    rental validations tests passing

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,22 +1,21 @@
 require "test_helper"
-# TODO these should pass after rentals controller methods are written
 describe Rental do
-  # describe "validations" do
-  #   let (:rental_one) {rentals(:rental_one)}
-  #   it "can be created" do
-  #     expect(rental_one.valid?).must_equal true
-  #   end
-  #
-  #   it "requires customer_id and video_id" do
-  #     required_fields = [:customer_id, :video_id]
-  #
-  #     required_fields.each do |field|
-  #       rental_one[field] = nil
-  #
-  #       expect(rental_one.valid?).must_equal false
-  #
-  #       rental_one.reload
-  #     end
-  #   end
-  # end
+  describe "validations" do
+    let (:rental_one) {rentals(:rental_one)}
+    it "can be created" do
+      expect(rental_one.valid?).must_equal true
+    end
+
+    it "requires customer_id and video_id" do
+      required_fields = [:customer_id, :video_id]
+
+      required_fields.each do |field|
+        rental_one[field] = nil
+
+        expect(rental_one.valid?).must_equal false
+
+        rental_one.reload
+      end
+    end
+  end
 end
