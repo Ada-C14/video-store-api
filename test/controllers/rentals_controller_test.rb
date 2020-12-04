e71437d1e40239604c27a5a420e2f7d935699e77
Ida <ida.goitom@yahoo.com>
2020-12-04 11:25:22 -0800

    rental checkout positive and negative cases passing

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,14 +1,44 @@
 require "test_helper"
 
 describe RentalsController do
-  it "must get index" do
-    get rentals_index_url
-    must_respond_with :success
-  end
 
-  it "must get show" do
-    get rentals_show_url
-    must_respond_with :success
-  end
+  describe "check out" do
+    it "creates a valid rental" do
+      customer = customers(:customer_one)
+      video = videos(:video_one)
 
+      rental_hash = {
+          customer_id: customer.id,
+          video_id: video.id,
+          returned: false,
+          check_out_date: Date.today,
+          due_date: Date.today + 7.days,
+      }
+
+      expect {
+        post checkout_path, params: rental_hash
+      }.must_change "Rental.count", 1
+
+      must_respond_with :ok
+    end
+
+    it "responds with a not found for an invalid customer id" do
+      video = videos(:video_one)
+      rental_hash = {
+          customer_id: nil,
+          video_id: video.id,
+          returned: false,
+          check_out_date: Date.today,
+          due_date: Date.today + 7.days
+      }
+
+      expect {
+        post checkout_path, params: rental_hash
+      }.wont_change "Rental.count"
+
+      must_respond_with :not_found
+      body = JSON.parse(response.body)
+      expect(body['errors']).must_equal ['Not Found']
+    end
+  end
 end
