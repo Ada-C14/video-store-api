5953368e6d8915b2ecbdb5a78d99c6d35a45f314
Taylor Tompkins <taylormililani@gmail.com>
2020-12-03 16:35:48 -0800

    fixed rental controller

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -17,7 +17,7 @@ class RentalsController < ApplicationController
       video = Video.find_by(id: rental.video_id)
 
 
-      render json: { customer_id: customer.id, video_id: video.id, title: video.title, name: customer.name, postal_code: customer.postal_code, due_date: rental.due_date },
+      render json: { customer_id: customer.id, video_id: video.id, title: video.title, name: customer.name, postal_code: customer.postal_code, due_date: rental.due_date, checkout_date: rental.checkout_date },
              status: :ok
     end
 
@@ -33,22 +33,22 @@ class RentalsController < ApplicationController
     end
 
     rental = Rental.new(
-        due_date: (Date.today + 7),
+        due_date: Date.today + 7,
         customer_id: customer.id,
         video_id: video.id,
-        videos_checked_out_count: customer.videos_checked_out_count + 1,
-        available_inventory: video.available_inventory - 1
+        checkout_date: Date.today
     )
 
     if rental.save
       # call method to increment customer videos checked out
-      rental.videos_checked_out_count = customer.toggle_up_video_count
+      customer.toggle_up_video_count
       # # call method to decrease available inventory
-      rental.available_inventory = video.toggle_down_inventory
-      rental.save
+      video.toggle_down_inventory
 
-      render json: rental.as_json(only: [:customer_id, :video_id, :due_date, :videos_checked_out_count, :available_inventory]),
-             status: :ok
+      render json: rental.as_json(only: [:customer_id, :video_id, :due_date]).merge(
+          videos_checked_out_count: customer.videos_checked_out_count,
+          available_inventory: video.available_inventory
+      ), status: :ok
     else
       render json: { errors: rental.errors.messages }, status: :not_found
     end
@@ -72,17 +72,14 @@ class RentalsController < ApplicationController
     end
 
     customer.toggle_down_video_count
-    customer.save
-    rental.assign_attributes(videos_checked_out_count: customer.videos_checked_out_count)
-
     video.toggle_up_inventory
-    video.save
-    rental.assign_attributes(available_inventory: video.available_inventory)
     rental.due_date = nil
     rental.save
 
-    render json: rental.as_json(only: [:customer_id, :video_id, :videos_checked_out_count, :available_inventory]),
-            status: :ok
+    render json: rental.as_json(only: [:customer_id, :video_id]).merge(
+        videos_checked_out_count: customer.videos_checked_out_count,
+        available_inventory: video.available_inventory
+    ), status: :ok
 
   end
 end
