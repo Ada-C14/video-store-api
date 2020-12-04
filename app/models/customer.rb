8f53be8689408d4702286cbcff1a54ce396dbc9f
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2020-12-03 22:02:18 -0800

    removed default scope for sorting bcz Sorting by ID is the rails default

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,8 +1,6 @@
 class Customer < ApplicationRecord
   has_many :videos
 
-  default_scope { order(id: :asc) }
-
   def checkout_update
     self.update(videos_checked_out_count: self.videos_checked_out_count + 1)
   end
