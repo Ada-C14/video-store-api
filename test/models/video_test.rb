4dd26104cd8a41281607f26f371159e3ff1a43ab
Anya Tokar <61166946+anyatokar@users.noreply.github.com>
2020-12-03 18:25:49 -0800

    video validation tests added and passing

--- test/models/video_test.rb
+++ test/models/video_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 describe Video do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  describe "validations" do
+    let (:black_widow) {videos(:black_widow)}
+    it "can be created" do
+      expect(black_widow.valid?).must_equal true
+    end
+
+    it "requires name address city state postal_code phone" do
+      required_fields = [:title, :available_inventory, :total_inventory, :overview, :release_date]
+
+      required_fields.each do |field|
+        black_widow[field] = nil
+
+        expect(black_widow.valid?).must_equal false
+
+        black_widow.reload
+      end
+    end
+  end
 end
