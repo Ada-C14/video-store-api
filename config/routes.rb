ab68c32e829c1405c54a249b1a78c89246a095b2
jwinchan <68631640+jwinchan@users.noreply.github.com>
2020-12-04 09:50:16 -0800

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :videos, only: [:index, :show, :create]
   resources :customers, only: [:index]
 
-  post "/rentals/check_in", to: "rentals#check_in", as: "check-in"
+  post "/rentals/check_out", to: "rentals#check_out", as: "check-out"
 end
