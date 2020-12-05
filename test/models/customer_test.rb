30b88df4811b88759e2870209f94442da8077eee
Anna Kim <anna.kim93@gmail.com>
2020-12-04 19:10:00 -0500

    add customer tests for custom model methods: check in and out

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,8 +1,8 @@
-require "test_helper"
+require 'test_helper'
 
 describe Customer do
-  describe "validations" do
-    it "validates that all fields are present" do
+  describe 'validations' do
+    it 'validates that all fields are present' do
       customer = customers(:customer_one)
 
       expect(customer.valid?).must_equal true
@@ -17,16 +17,16 @@ describe Customer do
       expect(customer.errors.messages[:name]).must_equal ["can't be blank"]
     end
 
-    it "fails validation if customer names are not unique" do
-      customer = Customer.find_by(name: "Simon Del Rosario")
-      customer2 = Customer.create(name: "Simon Del Rosario", registered_at: "Thur, 29 Apr 2015 07:54:14 -0700", address: "23424 weeee", city: "Lynnwood", state: "WA", postal_code: "98126", phone: "(123)423-4566", videos_checked_out_count: 3)
+    it 'fails validation if customer names are not unique' do
+      customer = Customer.find_by(name: 'Simon Del Rosario')
+      customer2 = Customer.create(name: 'Simon Del Rosario', registered_at: 'Thur, 29 Apr 2015 07:54:14 -0700', address: '23424 weeee', city: 'Lynnwood', state: 'WA', postal_code: '98126', phone: '(123)423-4566', videos_checked_out_count: 3)
 
       expect(customer2.valid?).must_equal false
       expect(customer2.errors.messages).must_include :name
-      expect(customer2.errors.messages[:name]).must_equal ["has already been taken"]
+      expect(customer2.errors.messages[:name]).must_equal ['has already been taken']
     end
 
-    it "fails validation if registered_at is missing" do
+    it 'fails validation if registered_at is missing' do
       customer = customers(:customer_one)
       customer.registered_at = nil
 
@@ -35,7 +35,7 @@ describe Customer do
       expect(customer.errors.messages[:registered_at]).must_equal ["can't be blank"]
     end
 
-    it "fails validation if address, city, state, and postal_code is missing" do
+    it 'fails validation if address, city, state, and postal_code is missing' do
       customer = customers(:customer_one)
       customer.address = nil
       customer.city = nil
@@ -63,23 +63,42 @@ describe Customer do
       expect(customer.errors.messages[:phone]).must_equal ["can't be blank"]
     end
 
-    it "fails validation if phone number is not unique" do
-      customer = Customer.find_by(phone: "(469) 734-9111")
-      customer2 = Customer.create(name: "Simon Del Rosario", registered_at: "Thur, 29 Apr 2015 07:54:14 -0700", address: "23424 weeee", city: "Lynnwood", state: "WA", postal_code: "98126", phone: "(469) 734-9111", videos_checked_out_count: 3)
+    it 'fails validation if phone number is not unique' do
+      customer = Customer.find_by(phone: '(469) 734-9111')
+      customer2 = Customer.create(name: 'Simon Del Rosario', registered_at: 'Thur, 29 Apr 2015 07:54:14 -0700', address: '23424 weeee', city: 'Lynnwood', state: 'WA', postal_code: '98126', phone: '(469) 734-9111', videos_checked_out_count: 3)
 
       expect(customer2.valid?).must_equal false
       expect(customer2.errors.messages).must_include :phone
-      expect(customer2.errors.messages[:phone]).must_equal ["has already been taken"]
+      expect(customer2.errors.messages[:phone]).must_equal ['has already been taken']
     end
 
-    it "must have a video_check_out_counter greater than or equal to 0" do
+    it 'must have a video_check_out_counter greater than or equal to 0' do
       customer = customers(:customer_one)
       customer.videos_checked_out_count = -1
 
       expect(customer.valid?).must_equal false
       expect(customer.errors.messages).must_include :videos_checked_out_count
-      expect(customer.errors.messages[:videos_checked_out_count]).must_equal ["must be greater than or equal to 0"]
+      expect(customer.errors.messages[:videos_checked_out_count]).must_equal ['must be greater than or equal to 0']
     end
   end
 
+  describe 'custom methods' do
+
+    let (:customer) {
+      Customer.first
+    }
+
+    describe 'check_in' do
+      it 'will decrease videos_checked_out_count by 1' do
+
+        expect{ customer.check_in }.must_change 'customer.videos_checked_out_count', -1
+      end
+    end
+
+    describe 'check_out' do
+      it 'will increase videos_checked_out_count by 1' do
+        expect{ customer.check_out }.must_change 'customer.videos_checked_out_count', 1
+      end
+    end
+  end
 end
