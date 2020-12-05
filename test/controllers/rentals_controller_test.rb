aa37bdbe1859ecb5980c03ba0480fe59a4e5b1a6
Alice D <alicedilanchian@gmail.com>
2020-12-04 19:07:46 -0500

    rentals controller tests - all fixed 0 failures, 0 errors, 0 skips

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,116 +1,120 @@
 require "test_helper"
 
 describe RentalsController do
+
+  before do
+    @customer = customers(:customer_one)
+    @video = videos(:wonder_woman)
+    @rental_hash = {
+        video_id: @video.id,
+        customer_id: @customer.id
+    }
+  end
+
   describe "check_out" do
-    before do
-      @rental_hash = {
-          video: videos(:wonder_woman),
-          customer: customers(:customer_one)
-      }
-    end
+
 
     it "can check_out a rental - increments rental count" do
       expect {post check_out_path, params: @rental_hash}.must_change "Rental.count", 1
     end
     it "can check_out a rental - decrement video available inventory" do
-      expect {post check_out_path, params: @rental_hash}.must_change "@rental_hash.video.available_inventory", -1
+      expect {post check_out_path, params: @rental_hash}.must_change "@video.reload.available_inventory", -1
     end
 
     it "can check_out a rental - increment customer checkout count" do
-      expect {post check_out_path, params: @rental_hash}.must_change "@rental_hash.customer.videos_checked_out_count", 1
+      count = @customer.videos_checked_out_count
+      post check_out_path, params: @rental_hash
+      @customer.reload
+      expect(@customer.videos_checked_out_count).must_equal count + 1
     end
 
     it "can check_out a rental - returns status ok, and expected body" do
       post check_out_path, params: @rental_hash
       body = JSON.parse(response.body)
+
+      due_date = DateTime.parse(body["due_date"])
+
       fields = ["customer_id", "video_id", "due_date", "videos_checked_out_count", "available_inventory"].sort
       expect(body.keys.sort).must_equal fields
-      expect(body["customer_id"]).must_equal "testtttt"
-      expect(body["video_id"]).must_equal "tesssttt"
-      expect(body["due_date"]).must_equal "tesssst"
-      expect(body["available_inventory"]).must_equal "tessst"
+      expect(body["customer_id"]).must_equal @customer.id
+      expect(body["video_id"]).must_equal @video.id
+      expect(due_date).must_be_close_to(DateTime.now + 7.days, 60)
+      expect(body["available_inventory"]).must_equal @video.available_inventory - 1
       must_respond_with :ok
     end
 
-    it "responds with a 404 when checking out a rental with non valid video" do
-      @rental_hash[:video] = nil
+    it "responds with a not found when checking out a rental with non valid video" do
+      @rental_hash[:video_id] = nil
       post check_out_path, params: @rental_hash
       body = JSON.parse(response.body)
       expect(body.keys).must_include "errors"
       expect(body["errors"]).must_include  "Not Found"
       must_respond_with :not_found
     end
-    it "responds with a 404 when checking out a rental with non valid customer" do
-      @rental_hash[:customer] = nil
+    it "responds with a not found when checking out a rental with non valid customer" do
+      @rental_hash[:customer_id] = nil
       post check_out_path, params: @rental_hash
       body = JSON.parse(response.body)
       expect(body.keys).must_include "errors"
       expect(body["errors"]).must_include  "Not Found"
       must_respond_with :not_found
     end
-    it "responds with a 404 when checking out a rental with available inventory less than 1" do
+    it "responds with a not found when checking out a rental with available inventory less than 1" do
       video = Video.create
       video.available_inventory = 0
       video.save
-      @rental_hash[:video] = video
+      @rental_hash[:video_id] = video
       post check_out_path, params: @rental_hash
       body = JSON.parse(response.body)
       expect(body.keys).must_include "errors"
       expect(body["errors"]).must_include  "Not Found"
       must_respond_with :not_found
     end
-
-
-
-
   end
 
   describe "check_in" do
-    before do
-      @rental_hash = {
-          video: videos(:wonder_woman),
-          customer: customers(:customer_one)
-      }
+
+    it "can check_in a rental - increment video available inventory" do
+      expect {post check_in_path, params: @rental_hash}.must_change "@video.reload.available_inventory", 1
     end
 
-    it "can check_out a rental - increment video available inventory" do
-      expect {post check_in_path, params: @rental_hash}.must_change "@rental_hash.video.available_inventory", 1
-    end
-
-    it "can check_out a rental - decrement customer checkout count" do
-      expect {post check_in_path, params: @rental_hash}.must_change "@rental_hash.customer.videos_checked_out_count", -1
+    it "can check_in a rental - decrement customer checkout count" do
+      count = @customer.videos_checked_out_count
+      post check_in_path, params: @rental_hash
+      @customer.reload
+      expect(@customer.videos_checked_out_count).must_equal count - 1
     end
 
     it "can check_in a rental - returns status ok, and expected body" do
       post check_in_path, params: @rental_hash
       body = JSON.parse(response.body)
-      fields = ["customer_id", "video_id", "due_date", "videos_checked_out_count", "available_inventory"].sort
+      fields = ["customer_id", "video_id", "videos_checked_out_count", "available_inventory"].sort
       expect(body.keys.sort).must_equal fields
-      expect(body["customer_id"]).must_equal "testtttt"
-      expect(body["video_id"]).must_equal "tesssttt"
-      expect(body["videos_checked_out_count"]).must_equal "tesssst"
-      expect(body["available_inventory"]).must_equal "tessst"
+      expect(body["customer_id"]).must_equal @customer.id
+      expect(body["video_id"]).must_equal @video.id
+      expect(body["videos_checked_out_count"]).must_equal @customer.videos_checked_out_count - 1
+      expect(body["available_inventory"]).must_equal @video.available_inventory + 1
       must_respond_with :ok
     end
 
 
-    it "responds with a 404 when checking in a rental with non valid video" do
-      @rental_hash[:video] = nil
+    it "responds with a not found when checking in a rental with non valid video" do
+      @rental_hash[:video_id] = nil
       post check_in_path, params: @rental_hash
       body = JSON.parse(response.body)
       expect(body.keys).must_include "errors"
       expect(body["errors"]).must_include  "Not Found"
       must_respond_with :not_found
     end
-    it "responds with a 404 when checking in a rental with non valid customer" do
-      @rental_hash[:customer] = nil
+    it "responds with a not found when checking in a rental with non valid customer" do
+      @rental_hash[:customer_id] = nil
       post check_out_path, params: @rental_hash
       body = JSON.parse(response.body)
       expect(body.keys).must_include "errors"
       expect(body["errors"]).must_include  "Not Found"
       must_respond_with :not_found
     end
-    it "responds with a 404 when checking in a rental with non valid rental" do
+    it "responds with a not found when checking in a rental with non valid rental" do
       @rental_hash = nil
       post check_out_path, params: @rental_hash
       body = JSON.parse(response.body)
